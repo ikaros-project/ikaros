b4f57cdb48b9fa4998fa929d890e3a062784acbd
ikaros-project <christian@ikaros-project.org>
2012-10-19 00:55:05 +0200

    Final Image object and profiling added

--- Source/WebUI/Classes/Lines.js
+++ Source/WebUI/Classes/Lines.js
@@ -1,18 +1,9 @@
-function Lines(p)
+function CLines(p)
 {
-	this.inited = false;
-    p.frame = (p.frame ? p.frame : 'none');
-	this.graph = new Graph(p, p.module+'.'+p.source);
-	this.LUT = makeLUTArray(p.color, ['yellow']);
-	this.stroke_width = (p.stroke_width ? p.stroke_width : 1);		
-	this.module = p.module;
-	this.source = p.source;
-	this.width = p.width;
-	this.height = p.height;
+    this.obj = 	new WebUICanvas(this, p);
+ 
     this.arrow = p.arrow;
-    this.polar = p.arrow;
-    
-	this.point = null;
+    this.polar = p.polar;
     
 	this.min = (p.min ? p.min : 0);
 	this.max = (p.max ? p.max : 1);
@@ -30,109 +21,30 @@ function Lines(p)
     
     this.flip_y_axis = p.flip_y_axis;
     
-    var defs = document.createElementNS(svgns, "defs");
-    var marker = document.createElementNS(svgns, "marker");
-    var path = document.createElementNS(svgns, "path");
-    
-    marker.setAttribute("id", "YellowTriangle");
-    marker.setAttribute("viewBox", "0 0 10 10");
-    marker.setAttribute("refX", "2");
-    marker.setAttribute("refY", "5");
-    marker.setAttribute("markerUnits", "strokeWidth");
-    marker.setAttribute("markerWidth", "8");
-    marker.setAttribute("markerHeight", "6");
-    marker.setAttribute("stroke", "yellow");
-    marker.setAttribute("fill", "yellow");
-    marker.setAttribute("orient", "auto");
-    
-    path.setAttribute("d","M 0 0 L 10 5 L 0 10 z");
-    
-    marker.appendChild(path);
-    defs.appendChild(marker);
-    this.graph.svg.appendChild(defs);
+    console.log(this.flip_y_axis);
 
 	usesData(this.module, this.source);
+
+    this.context.clearRect(0, 0, this.width, this.height);
+    this.context.fillStyle="none";
+    this.context.fillRect(0, 0, this.width, this.height);
+    this.context.translate(0.5, 0.5);
 }
 
 
 
-Lines.prototype.Init = function(data)
-{
-    var d = data[this.module][this.source];
-    
-    this.inited = true;
-    
-    this.point = new Array(0);
-    this.sizex = d[0].length;
-    this.sizey = d.length;
-    
-    if(this.flip_y_axis)
-    {
-        for(var i=0; i<this.sizey; i++)
-        {
-            var x2, y2;
-            if(this.polar)
-            {
-                var a = d[i][2];
-                x2 = d[i][0] + this.length*Math.cos(a);
-                y2 = d[i][1] + this.length*Math.sin(a);
-            }
-            else
-            {
-                x2 = d[i][2];
-                y2 = d[i][3];
-            }
-            var l = this.graph.AddLine(
-                                       (d[i][0]-this.min_x)*this.scale_x * this.width, 
-                                       this.height-(d[i][1]-this.min_y)*this.scale_y * this.height, 
-                                       (x2-this.min_x)*this.scale_x * this.width, 
-                                       this.height-(y2-this.min_y)*this.scale_y * this.height, 
-                                       this.LUT[i % this.LUT.length], this.stroke_width);
-            if(this.arrow =="yes")
-                l.setAttribute("marker-end", "url(#YellowTriangle)");
-            this.point.push(l);
-        }
-    }
-    else
-    {
-        for(var i=0; i<this.sizey; i++)
-        {
-            var x2, y2;
-            if(this.polar)
-            {
-                var a = d[i][2];
-                x2 = d[i][0] + this.length*Math.cos(a);
-                y2 = d[i][1] + this.length*Math.sin(a);
-            }
-            else
-            {
-                x2 = d[i][2];
-                y2 = d[i][3];
-            }
-            var l = this.graph.AddLine(
-                                       (d[i][0]-this.min_x)*this.scale_x * this.width, 
-                                       (d[i][1]-this.min_y)*this.scale_y * this.height, 
-                                       (x2-this.min_x)*this.scale_x * this.width, 
-                                       (y2-this.min_y)*this.scale_y * this.height, 
-                                       this.LUT[i % this.LUT.length], this.stroke_width);
-            if(this.arrow =="yes")
-                l.setAttribute("marker-end", "url(#YellowTriangle)");
-            this.point.push(l);
-        }
-    }
-}
-
-
-
-Lines.prototype.Update = function(data)
+CLines.prototype.Update = function(data)
 {
     var d = data[this.module];
     if(!d) return;
     d = d[this.source]
     if(!d) return;
     
-    if(!this.inited) this.Init(data);
-        
+    this.sizex = d[0].length;
+    this.sizey = d.length;
+
+    this.context.clearRect(0, 0, this.width, this.height);
+    
     if(this.flip_y_axis)
     {
         for(var i=0; i<this.sizey; i++)
@@ -149,11 +61,25 @@ Lines.prototype.Update = function(data)
                 x2 = d[i][2];
                 y2 = d[i][3];
             }
-
-            this.point[i].setAttribute("x1", (d[i][0]-this.min_x)*this.scale_x * this.width);
-            this.point[i].setAttribute("y1", this.height-(d[i][1]-this.min_y)*this.scale_y * this.height);
-            this.point[i].setAttribute("x2", (x2-this.min_x)*this.scale_x * this.width);
-            this.point[i].setAttribute("y2", this.height-(y2-this.min_y)*this.scale_y * this.height);
+            
+            if(this.arrow)
+            {
+                this.context.strokeStyle = this.LUT[i % this.LUT.length];
+                this.context.fillStyle = this.LUT[i % this.LUT.length];
+                this.context.lineWidth = this.stroke_width;
+                this.context.drawLineArrow(
+                                           (d[i][0]-this.min_x)*this.scale_x * this.width, this.height-(d[i][1]-this.min_y)*this.scale_y * this.height,
+                                           (x2-this.min_x)*this.scale_x * this.width, this.height-(y2-this.min_y)*this.scale_y * this.height);
+            }
+            else
+            {
+                this.context.beginPath();
+                this.context.strokeStyle = this.LUT[i % this.LUT.length];
+                this.context.lineWidth = this.stroke_width;
+                this.context.moveTo((d[i][0]-this.min_x)*this.scale_x * this.width, this.height-(d[i][1]-this.min_y)*this.scale_y * this.height);
+                this.context.lineTo((x2-this.min_x)*this.scale_x * this.width, this.height-(y2-this.min_y)*this.scale_y * this.height);
+                this.context.stroke();
+            }
         }
     }
     else
@@ -173,10 +99,24 @@ Lines.prototype.Update = function(data)
                 y2 = d[i][3];
             }
             
-            this.point[i].setAttribute("x1", (d[i][0]-this.min_x)*this.scale_x * this.width);
-            this.point[i].setAttribute("y1", (d[i][1]-this.min_y)*this.scale_y * this.height);
-            this.point[i].setAttribute("x2", (x2-this.min_x)*this.scale_x * this.width);
-            this.point[i].setAttribute("y2", (y2-this.min_y)*this.scale_y * this.height);
+            if(this.arrow)
+            {
+                this.context.strokeStyle = this.LUT[i % this.LUT.length];
+                this.context.fillStyle = this.LUT[i % this.LUT.length];
+                this.context.lineWidth = this.stroke_width;
+                this.context.drawLineArrow(
+                              (d[i][0]-this.min_x)*this.scale_x * this.width, (d[i][1]-this.min_y)*this.scale_y * this.height,
+                              (x2-this.min_x)*this.scale_x * this.width, (y2-this.min_y)*this.scale_y * this.height);
+            }
+            else
+            {
+                this.context.beginPath();
+                this.context.moveTo((d[i][0]-this.min_x)*this.scale_x * this.width, (d[i][1]-this.min_y)*this.scale_y * this.height);
+                this.context.lineTo((x2-this.min_x)*this.scale_x * this.width, (y2-this.min_y)*this.scale_y * this.height);
+                this.context.lineWidth = this.stroke_width;
+                this.context.strokeStyle = this.LUT[i % this.LUT.length];
+                this.context.stroke();
+            }
         }
     }
 }
