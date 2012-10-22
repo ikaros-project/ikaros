05546cb50ec6e69b8f8fb8715769a044e052377e
ikaros-project <christian@ikaros-project.org>
2012-10-22 10:38:54 +0200

    Edge detector paths corrected

--- Source/Modules/VisionModules/ImageOperators/EdgeDetectors/HysteresisThresholding/HysteresisThresholding.cc
+++ Source/Modules/VisionModules/ImageOperators/EdgeDetectors/HysteresisThresholding/HysteresisThresholding.cc
@@ -97,6 +97,6 @@ HysteresisThresholding::Tick()
                                 output[j+jj][i+ii] = 1;
 }
 
-static InitClass init("HysteresisThresholding", &HysteresisThresholding::Create, "Source/Modules/VisionModules/ImageOperators/EdgeDetector/HysteresisThresholding/");
+static InitClass init("HysteresisThresholding", &HysteresisThresholding::Create, "Source/Modules/VisionModules/ImageOperators/EdgeDetectors/HysteresisThresholding/");
 
 
