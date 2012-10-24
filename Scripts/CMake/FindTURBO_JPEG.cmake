3d4d39a7e8017055e3c0f37f186c82bd139ec30d
Birger Johansson <robotlab@asip.lucs.lu.se>
2012-10-24 11:15:09 +0200

    Adding FindTURBOJPEG (OSX)
    Not used in linux as FindJPEG will found TurboJPEG

---
+++ Scripts/CMake/FindTURBO_JPEG.cmake
@@ -0,0 +1,42 @@
+# Find turbo jpeg for OSX.
+# 
+# This module defines
+# TURBO_JPEG_INCLUDE_DIR
+# TURBO_JPEG_LIBRARIES
+# TURBO_JPEG_FOUND
+
+
+	find_path(TURBO_JPEG_INCLUDE_DIR
+    NAMES
+      jconfig.h jerror.h jmorecfg.h jpeglib.h turbojpeg.h
+    PATHS
+		/usr/local/opt/jpeg-turbo/include/
+  	)
+  
+	find_library(TURBO_JPEG_LIBRARIES
+    NAMES
+    	jpeg
+    PATHS
+    	/usr/local/opt/jpeg-turbo/lib
+	)
+  
+	if (TURBO_JPEG_LIBRARIES)
+    	set(TURBO_JPEG_FOUND TRUE)
+  	endif (TURBO_JPEG_LIBRARIES)
+ 
+ 
+  	set(TURBO_JPEG_INCLUDE_DIRS
+    	${TURBO_JPEG_INCLUDE_DIR}
+  	)
+  
+    if (TURBO_JPEG_FOUND)
+    	set(TURBO_JPEG_LIBRARIES
+      	${TURBO_JPEG_LIBRARIES}
+    	)
+    endif(TURBO_JPEG_FOUND)
+    
+    if (TURBO_JPEG_FOUND)
+    	message(STATUS "Found TurboJPEG: ${TURBO_JPEG_LIBRARIES}")
+  	else (TURBO_JPEG_FOUND)
+    	message(STATUS "Could not find TurboJPEG")
+  	endif (TURBO_JPEG_FOUND)
