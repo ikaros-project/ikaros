66c4bf7c1f87efd67cb98da9f5e50e324cbc4cc1
ikaros-project <christian@ikaros-project.org>
2013-10-21 01:37:25 +0200

    MedianFilter first commit

--- Source/Kernel/IKAROS_Math.cc
+++ Source/Kernel/IKAROS_Math.cc
@@ -2741,13 +2741,14 @@ namespace ikaros
     float *
     sort(float * a, int size)   // insertion sort
     {
-        int t, i , j;
-        for(i = 0; i < size; i++)
+        int i , j;
+        float t;
+        for(i = 1; i < size; i++)
         {
             t = a[i];
-            for(j = i-1; j >= 0 && t < a[j]; j--)
-                a[j+1] = a[j];
-            a[j+1] = t;
+            for(j = i; j > 0 && t < a[j-1]; j--)
+                a[j] = a[j-1];
+            a[j] = t;
         }
         return a;
     }
