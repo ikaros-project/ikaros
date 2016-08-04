f4416e8c41eaff239149031bd26fa94a5fe853fb
Birger Johansson <m@birgerjohansson.com>
2016-08-04 12:50:15 +0200

    Rewriting FFmpeg module as for new FFmpeg API

--- Source/Modules/IOModules/FileInput/InputVideoFile/InputVideoFile.cc
+++ Source/Modules/IOModules/FileInput/InputVideoFile/InputVideoFile.cc
@@ -1,7 +1,7 @@
 //
 //	  InputVideoFile.cc		This file is a part of the IKAROS project
 //
-//    Copyright (C) 2012 Birger Johansson
+//    Copyright (C) 2016 Birger Johansson
 //
 //    This program is free software; you can redistribute it and/or modify
 //    it under the terms of the GNU General Public License as published by
@@ -19,27 +19,16 @@
 //
 //    See http://www.ikaros-project.org/ for more information.
 //
-
-// Based on FFMpeg tutorial http://dranger.com/ffmpeg/
-
+//
 
 #include "InputVideoFile.h"
 
-//#define MY_PARALLEL_CODE
 using namespace ikaros;
-
-
-#ifdef MY_PARALLEL_CODE
-#include <thread>
-
-using namespace std;
-
-#endif
-
 
 InputVideoFile::InputVideoFile(Parameter * p):
 Module(p)
 {
+    
     filename = GetValue("filename");
     if (filename == NULL)
     {
@@ -48,21 +37,20 @@ Module(p)
     }
     
     loop = GetBoolValue("loop", false);
-    
     printInfo = GetBoolValue("info", false);
     
     // Register all formats and codecs
     av_register_all();
     
     /// Open video file
-    if(avformat_open_input(&pFormatCtx, filename, NULL, NULL) != 0)
+    if(avformat_open_input(&input_format_context, filename, NULL, NULL) != 0) // Allocating input_format_context
     {
         Notify(msg_fatal_error, "Could not open file.\n");
         return;
     }
     
     /// Retrieve stream information
-    if(avformat_find_stream_info(pFormatCtx, NULL) < 0)
+    if(avformat_find_stream_info(input_format_context, NULL) < 0)
     {
         Notify(msg_fatal_error, "Couldn't find stream information\n");
         return;
@@ -70,55 +58,81 @@ Module(p)
     
     /// Dump information about file onto standard error
     if (printInfo)
-        av_dump_format(pFormatCtx, 0, filename, 0);
+        av_dump_format(input_format_context, 0, filename, 0);
     
     /// Find the first video stream
-    videoStreamIdx=-1;
-    for(int i=0; i<pFormatCtx->nb_streams; i++)
-        if(pFormatCtx->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO) { //CODEC_TYPE_VIDEO
-            videoStreamIdx=i;
+    videoStreamId = -1;
+    for(int i=0; i<input_format_context->nb_streams; i++)
+        if(input_format_context->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_VIDEO) {
+            videoStreamId=i;
             break;
         }
-    if(videoStreamIdx==-1)
+    if(videoStreamId==-1)
     {
         Notify(msg_fatal_error, "Didn't find a video stream\n");
         return;
     }
     
-    /// Get a pointer to the codec context for the video stream
-    pCodecCtx = pFormatCtx->streams[videoStreamIdx]->codec;
+    /// Find the first audio stream
+    audioStreamId = -1;
+    for(int i=0; i<input_format_context->nb_streams; i++)
+        if(input_format_context->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_AUDIO) {
+            audioStreamId=i;
+            break;
+        }
+    if(audioStreamId==-1)
+    {
+        Notify(msg_fatal_error, "Didn't find a Audio stream\n");
+        return;
+    }
+    
+    // Decoding video
     
     /// Find the decoder for the video stream
-    pCodec = avcodec_find_decoder( pCodecCtx->codec_id);
-    if(pCodec==NULL) {
+    input_codec = avcodec_find_decoder(input_format_context->streams[videoStreamId]->codecpar->codec_id);
+    if(input_codec==NULL) {
         fprintf(stderr, "Unsupported codec!\n");
         return; // Codec not found
     }
+    
+    // Creating decoding context
+    avctx = avcodec_alloc_context3(input_codec);
+    if (!avctx) {
+        fprintf(stderr, "Could not allocate a decoding context\n");
+        avformat_close_input(&input_format_context);
+        return;
+    }
+    
+    // Setting parmeters to context?
+    int error = avcodec_parameters_to_context(avctx, (*input_format_context).streams[videoStreamId]->codecpar);
+    if (error < 0) {
+        avformat_close_input(&input_format_context);
+        avcodec_free_context(&avctx);
+        return;
+    }
+    
     /// Open codec
-    if( avcodec_open2(pCodecCtx, pCodec, NULL) < 0 )
+    if( avcodec_open2(avctx, input_codec, NULL) < 0 )
     {
         Notify(msg_fatal_error, "Could not open codec\n");
         return;
     }
     
-    /// Allocate video frame
-    pFrame = av_frame_alloc();
+    inputFrame = av_frame_alloc();    // Input
+    outputFrame = av_frame_alloc();   // Output (after resize and convertions)
     
-    /// Allocate an AVFrame structure
-    pFrameRGB = av_frame_alloc();
-    if(pFrameRGB==NULL)
+    if(inputFrame == NULL || outputFrame==NULL)
     {
         Notify(msg_fatal_error, "Could not allocate AVFrame\n");
         return;
     }
     
-    // Check native size
-    
+    // Is the output size set in ikc file? If not use native size
     size_x = GetIntValue("size_x");
     size_y = GetIntValue("size_y");
     
-    native_size_x = int(pCodecCtx->width);
-    native_size_y = int(pCodecCtx->height);
+    native_size_x = int(avctx->width);
+    native_size_y = int(avctx->height);
     
     if(size_x == 0 || size_y == 0)
     {
@@ -127,12 +141,11 @@ Module(p)
     }
     
     /// Determine required buffer size and allocate buffer
-    numBytes = avpicture_get_size(AV_PIX_FMT_RGB24, size_x, size_y);
+    numBytes = av_image_get_buffer_size(AV_PIX_FMT_RGB24, size_x, size_y,1);
     buffer = (uint8_t *) av_malloc(numBytes*sizeof(uint8_t));
     
-    /// Assign appropriate parts of buffer to image planes in pFrameRGB
-    avpicture_fill((AVPicture *)pFrameRGB, buffer, AV_PIX_FMT_RGB24,
-                   size_x, size_y);
+    /// Assign appropriate parts of buffer to image planes in outputFrame
+    av_image_fill_arrays(outputFrame->data,outputFrame->linesize, buffer, AV_PIX_FMT_RGB24,size_x, size_y,1);
     
     AddOutput("INTENSITY", size_x, size_y);
     AddOutput("RED", size_x, size_y);
@@ -154,137 +167,65 @@ InputVideoFile::Init()
     restart[0]  = -1; // indicates first tick
 }
 
+
+// The flush packet is a non-NULL packet with size 0 and data NULL
+int
+InputVideoFile::decode(AVCodecContext *avctx, AVFrame *frame, int *got_frame, AVPacket *pkt)
+{
+    int ret;
+    *got_frame = 0;
+    if (pkt) {
+        ret = avcodec_send_packet(avctx, pkt);
+        // In particular, we don't expect AVERROR(EAGAIN), because we read all
+        // decoded frames with avcodec_receive_frame() until done.
+        if (ret < 0)
+            return ret == AVERROR_EOF ? 0 : ret;
+    }
+    
+    ret = avcodec_receive_frame(avctx, frame);
+    if (ret < 0 && ret != AVERROR(EAGAIN) && ret != AVERROR_EOF)
+        return ret;
+    if (ret >= 0)
+        *got_frame = 1;
+    
+    return 0;
+}
+
 void
 InputVideoFile::Tick()
 {
     const float c13 = 1.0/3.0;
     const float c1255 = 1.0/255.0;
     restart[0] = (restart[0] == -1 ? 1 : 0);
-    frameFinished = false;
     
-    while (!frameFinished)
+    int gotFrame = 0;
+    while (!gotFrame)                                           // Keep reading from source until we get a video packet
     {
-        if(av_read_frame(pFormatCtx, &packet)>=0)
+        if(av_read_frame(input_format_context, &packet)>=0)     // Read packet from source
         {
-            // Is this a packet from the video stream?
-            if(packet.stream_index==videoStreamIdx)
+            if(packet.stream_index==videoStreamId)              // Is this a packet from the video stream?
             {
-                
-                /// Decode video frame
-                avcodec_decode_video2(pCodecCtx, pFrame, &frameFinished, &packet);
-                
-                // Did we get a video frame?
-                if(frameFinished) {
-                    
+                decode(avctx, inputFrame, &gotFrame, &packet);
+                if (gotFrame)                                   // Decode gave us a frame
+                {
+                    // Convert teh frame to AV_PIX_FMT_RGB24 format
                     static struct SwsContext *img_convert_ctx;
-                    
-                    img_convert_ctx = sws_getCachedContext(img_convert_ctx,pCodecCtx->width, pCodecCtx->height,
-                                                           pCodecCtx->pix_fmt,
+                    img_convert_ctx = sws_getCachedContext(img_convert_ctx,avctx->width, avctx->height,
+                                                           avctx->pix_fmt,
                                                            size_x, size_y, AV_PIX_FMT_RGB24,
                                                            SWS_BICUBIC, NULL, NULL, NULL);
                     
-                    
-                    sws_scale(img_convert_ctx, (const uint8_t * const *)pFrame->data,
-                              pFrame->linesize, 0, pCodecCtx->height,
-                              pFrameRGB->data, pFrameRGB->linesize);
-                    
-                    unsigned char * data = pFrameRGB->data[0];
-                    
-#ifdef MY_PARALLEL_CODE
-                    //printf("Parallel Code\n");
+                    // Scale the image
+                    sws_scale(img_convert_ctx, (const uint8_t * const *)inputFrame->data,
+                              inputFrame->linesize, 0, avctx->height,
+                              outputFrame->data, outputFrame->linesize);
 
-                    // Parallel  Code
-                    //
-                    // iMac 8 threads
-//                IKAROS: Reading XML file "InputVideoFile_test.ikc".
-//                IKAROS: Start
-//                IKAROS: Stop (2000 ticks, 13.90 s, 143.90 ticks/s, 0.007 s/tick)
-//                IKAROS:
-//                IKAROS: Time (ms):
-//                IKAROS: -------------------
-//                IKAROS: Modules:   13743.55
-//                IKAROS: Other:       154.69
-//                IKAROS: -------------------
-//                IKAROS: Total:     13898.24
-//                IKAROS:
-//                IKAROS: Time in Each Module:
-//                IKAROS:
-//                IKAROS: Module              Class                    Count  Avg (ms)    Time %
-//                IKAROS: ----------------------------------------------------------------------
-//                IKAROS: InputVideoFile      InputVideoFile            2000      6.87     100.0
-//                IKAROS: ----------------------------------------------------------------------
-//                IKAROS: Note: Time is real-time, not time in thread.
-//                IKAROS: 
-
-                    
-                    const int nrOfCores = 8; //thread::hardware_concurrency();
-                    // Create some threads pointers
-                    thread t[nrOfCores];
-                    //printf("Number of threads %i\n",thread::hardware_concurrency());
-                    int span = size_y/nrOfCores;
-                    
-                    for(int j=0; j<nrOfCores; j++)
-                    {
-                        t[j] = thread([&,j]()
-                                      {
-                                          //printf("I am thread %i and I will take care of %i to %i\n",j, j*span,j*span+span);
-                                          
-                                          for(int y=j*span; y<j*span+span; y++)
-                                          {
-                                              for(int x=0; x<size_x; x++)
-                                              {
-                                                  int yLineSize = y*pFrameRGB->linesize[0];
-                                                  int y1 = y*size_x;
-                                                  int xy = x + y1;
-                                                  int x3  = x*3;
-                                                  intensity[xy] 	=   red[xy]       = c1255*data[yLineSize+x3+0];
-                                                  intensity[xy] 	+=  green[xy]     = c1255*data[yLineSize+x3+1];
-                                                  intensity[xy] 	+=  blue[xy]      = c1255*data[yLineSize+x3+2];
-                                                  intensity[xy]     *=  c13;
-                                                  
-                                              }
-                                          }
-                                      });
-                        
-                    }
-                    
-                    for(int j=0; j<nrOfCores; j++)
-                    {
-                        t[j].join();
-                    }
-#else
-                    
-                    //printf("Serial Code\n");
-                    // Serial Code
-                    //
-                    // iMac 1 thread
-                    
-//                IKAROS: Reading XML file "InputVideoFile_test.ikc".
-//                IKAROS: Start
-//                IKAROS: Stop (2000 ticks, 26.42 s, 75.70 ticks/s, 0.013 s/tick)
-//                IKAROS:
-//                IKAROS: Time (ms):
-//                IKAROS: -------------------
-//                IKAROS: Modules:   26202.73
-//                IKAROS: Other:       215.61
-//                IKAROS: -------------------
-//                IKAROS: Total:     26418.34
-//                IKAROS:
-//                IKAROS: Time in Each Module:
-//                IKAROS:
-//                IKAROS: Module              Class                    Count  Avg (ms)    Time %
-//                IKAROS: ----------------------------------------------------------------------
-//                IKAROS: InputVideoFile      InputVideoFile            2000     13.10     100.0
-//                IKAROS: ----------------------------------------------------------------------
-//                IKAROS: Note: Time is real-time, not time in thread.
-                    
-                    
-                    
-                    // Write pixel data
+                    // Put data in ikaros output
+                    unsigned char * data = outputFrame->data[0];
                     for(int y=0; y<size_y; y++)
                         for(int x=0; x<size_x; x++)
                         {
-                            int yLineSize = y*pFrameRGB->linesize[0];
+                            int yLineSize = y*outputFrame->linesize[0];
                             int y1 = y*size_x;
                             int xy = x + y1;
                             int x3  = x*3;
@@ -293,22 +234,16 @@ InputVideoFile::Tick()
                             intensity[xy] 	+=  blue[xy]      = c1255*data[yLineSize+x3+2];
                             intensity[xy]   *=  c13;
                         }
-#endif
-                    
                 }
-                
             }
-            // Free the packet that was allocated by av_read_frame
-            av_free_packet(&packet);
         }
-        
         else
         {
             if (loop)
             {
                 restart[0] = 1;
-                
-                if(av_seek_frame(pFormatCtx, 0, 0, AVSEEK_FLAG_ANY) < 0)
+                avcodec_flush_buffers(avctx);                                       // Need to be done before rewinding
+                if(av_seek_frame(input_format_context, 0, 0, AVSEEK_FLAG_ANY) < 0)  // Rewind movie
                     printf("error while seeking\n");
             }
             else
@@ -323,19 +258,10 @@ InputVideoFile::Tick()
 
 InputVideoFile::~InputVideoFile()
 {
-    // Free the RGB image
     av_free(buffer);
-    av_free(pFrameRGB);
-    
-    // Free the YUV frame
-    av_free(pFrame);
-    
-    // Close the codec
-    avcodec_close(pCodecCtx);
-    
-    // Close the video file
-    avformat_close_input(&pFormatCtx);
+    av_free(outputFrame);
+    av_free(inputFrame);
+    avcodec_close(avctx);
+    avformat_close_input(&input_format_context);
 }
 static InitClass init("InputVideoFile", &InputVideoFile::Create, "Source/Modules/IOModules/FileInput/InputVideoFile/");
-
-
