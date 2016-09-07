549b595505aad064615f6e39686a9f638dc82c1c
Birger Johansson <m@birgerjohansson.com>
2016-09-07 10:54:16 +0200

    Massive update of the Dynamixel module
    Fixing bug #126
    Warnings for #130
    Fixing public issue 6
    Fixing #163
    And many more things.
    
    Tested Joint mode and wheel mode. (Multi-turn is not yet implemented).
    Tested using AX-12, MX-106 and MX-28.

--- Source/Modules/RobotModules/Dynamixel/DynamixelConfigure.cc
+++ Source/Modules/RobotModules/Dynamixel/DynamixelConfigure.cc
@@ -30,6 +30,8 @@ using namespace ikaros;
 DynamixelConfigure::DynamixelConfigure(Parameter * p):
 Module(p)
 {
+    forceModel = GetIntValue("force_model"); // Overide the read model
+
     device = GetValue("device");
     if(!device)
     {
@@ -37,7 +39,7 @@ Module(p)
         size = 0;
         return;
     }
-    
+    std::string csvPath = GetClassPath();
     
     // Reset mode
     resetMode = GetBoolValue("reset_mode");
@@ -87,7 +89,7 @@ Module(p)
                 size = 0;
                 for(int j=0; j<max_servo_id; j++)
                 {
-                    if(com->Ping(j))
+                    if(com->Ping(j) > 0)
                     {
                         printf(" %i ",j);
                         size += 1;
@@ -99,10 +101,8 @@ Module(p)
             }
         }
         Notify(msg_terminate, "Scan completed. quiting ikaros...");
-        
     }
     
-    //
     baud_rate = GetIntValue("baud_rate");
     com = new DynamixelComm(device, baud_rate);
     if(!com)
@@ -125,17 +125,14 @@ Module(p)
         if(com->Ping(i) > 0)
         {
             printf(" %i ",i);
-            servo[size] = new DynamixelServo(com, i);
+            servo[size] = new DynamixelServo(com, i, csvPath.c_str(),forceModel);
             size += 1;
         }
     }
     printf("\n\n");
     
-    // Count servos
-    servos = 0;
-    for(int i=0; i<size; i++)
-        if(servo[i])
-            servos++;
+    // Number of servos
+    servos = size;
     
     // Allocate servo maps and parameter structures
     servo_index  = new int [servos];
@@ -146,15 +143,14 @@ Module(p)
     // Connecting memory to servo memory
     for(int i=0; i<size; i++)
         if(servo[i])
-            DynamixelMemoeries[i] = servo[i]->DynamixelMemory;
+            DynamixelMemoeries[i] = servo[i]->dynamixelMemory;
     
-    // filling servo_index and servo_id. Example: servo_index [1 2 3 4 5] and servo_id[3 2 10 1 3]
     int j=0;
     for(int i=0; i<size; i++)
         if(servo[i])
         {
-            servo_index[i] = j++;
-            servo_id[i] = servo[i]->controlTable.GetInt("ID");
+            servo_id[j] = servo[i]->extraInfo.GetInt("ID");
+            servo_index[j++] = i;
         }
     
     // If we did not find any servos kill ikaros.
@@ -163,16 +159,15 @@ Module(p)
     
     changeAdress = GetIntValue("adress");
     newValue = GetIntValue("value");
+    
+
 }
 
 void
 DynamixelConfigure::PrintChange(int i)
 {
-    if(servo[i])
-    {
         // Print changes for a single servo.
-        printf("\n\n(ID:%i) %s %i -> %i: ", servo[i]->controlTable.GetInt("ID"), servo[i]->ctable[changeAdress].Name.c_str(), servo[i]->GetValueAtAdress(changeAdress), newValue);
-    }
+        printf("\n\n(ID:%i) %s %i -> %i: ", servo[i]->extraInfo.GetInt("ID"), servo[i]->controlTable[changeAdress].Name.c_str(), servo[i]->GetValueAtAdress(changeAdress), newValue);
 }
 
 void
@@ -200,11 +195,60 @@ DynamixelConfigure::Init()
     changeModeOut = GetOutputArray("CHANGE_MODE");
     blink = 0;
     
-    // Find out where to look to ikaros output data from Dynamixel memory
-    for (int i = 0; i < size; i++)
-        for (int j = 0; j < 128; j++)
-            if (servo[i]->ctable[j].IkarosInputs >= 0)
-                ikarosInBind[servo[i]->ctable[j].IkarosInputs] = servo[i]->ctable[j].Adress;
+    
+    // OBS ORDER OF MATRIXES IS [Paramter][Servo]
+    // Create memory for binding map. These are specified in the csv file and correspond to where ikaros should look for IO in the dynamixel memory block
+    ikarosInBind = new int * [10];
+    for(int i=0; i<10; i++)
+        ikarosInBind[i] = new int [servos];
+    
+    ikarosOutBind = new int * [16];
+    for(int i=0; i<16; i++)
+        ikarosOutBind[i] = new int [servos];
+    
+    // Reset array
+    for (int i = 0; i < 10; i++)
+        for (int j = 0; j < servos; j++)
+            ikarosInBind[i][j] = -1;
+    for (int i = 0; i < 16; i++)
+        for (int j = 0; j < servos; j++)
+            ikarosOutBind[i][j] = -1;
+    
+    // Fill in where to look for data
+    for (int i = 0; i < servos; i++)
+        for (int j = 0; j < C_TABLE_SIZE; j++)
+        {
+            if (servo[servo_index[i]]->controlTable[j].IkarosInputs >= 0)
+                ikarosInBind[servo[servo_index[i]]->controlTable[j].IkarosInputs][i] = servo[servo_index[i]]->controlTable[j].Adress;
+            if (servo[servo_index[i]]->controlTable[j].IkarosOutputs >= 0)
+                ikarosOutBind[servo[servo_index[i]]->controlTable[j].IkarosOutputs][i] = servo[servo_index[i]]->controlTable[j].Adress;
+        }
+    
+    // OBS ORDER OF MATRIXES IS [inputs][Servo]
+    // Create memory size map
+    parameterInSize = new int * [10];
+    for(int i=0; i<10; i++)
+        parameterInSize[i] = new int [servos];
+    
+    for(int i=0; i<10; i++)
+        for(int j=0; j<servos; j++)
+            if(ikarosInBind[i][j]!= -1)
+                parameterInSize[i][j] =  servo[servo_index[j]]->controlTable[ikarosInBind[i][j]].Size;
+            else
+                parameterInSize[i][j] =  -1;
+    
+    // Do not allow mixed protocols
+    protocol = -1;
+    if (servos > 0)
+        for (int i = 0; i < servos; i++)
+        {
+            if (protocol == -1 && servo[servo_index[i]]->protocol != 0)
+                protocol = servo[servo_index[i]]->protocol; // Set protocol.
+            
+            if (protocol != servo[servo_index[i]]->protocol)
+                Notify(msg_fatal_error, "Dynamixel uses different protocols.\n\n");
+        }
+    PrintAll();
 }
 
 DynamixelConfigure::~DynamixelConfigure()
@@ -213,93 +257,126 @@ DynamixelConfigure::~DynamixelConfigure()
     Timer timer;
     for(int i=0; i<size; i++)
         if(servo[i])
-            servo[i]->SetValueAtAdress(ikarosInBind[IK_IN_LED], 0);
-    com->SyncWriteWithIdRange(servo_id, servo[0]->Protocol, DynamixelMemoeries, ikarosInBind[IK_IN_LED], ikarosInBind[IK_IN_LED], size);
+            servo[i]->SetValueAtAdress(ikarosInBind[IK_IN_LED][i], 0);
+    com->WriteToServo(servo_id, protocol, DynamixelMemoeries, ikarosInBind[IK_IN_LED], parameterInSize[IK_IN_LED], size);
     timer.Sleep(200);
     
     
-    // Delete dynamixel memory buffert
-    delete DynamixelMemoeries;
-    
     // Free memory
+    for(int i=0; i<10; i++)
+        delete(ikarosInBind[i]);
+    delete(ikarosInBind);
+    for(int i=0; i<10; i++)
+        delete(parameterInSize[i]);
+    delete(parameterInSize);
+    for(int i=0; i<16; i++)
+        delete(ikarosOutBind[i]);
+    delete(ikarosOutBind);
+    delete DynamixelMemoeries;
     delete servo_index;
     delete servo_id;
     delete com;
-    delete servo; // Also delete servo's memorybuffer
+    delete servo; // Also deletes servo's memorybuffer
 }
 
 void
 DynamixelConfigure::Tick()
 {
     Timer timer;
+    int selectedServo = int(active[0]);
     
-    if (int(active[0]) < 0)
-        active[0] = 0;
+    if (selectedServo < 0)
+        selectedServo = 0;
     
-    if (int(active[0]) >= size)
-        active[0] = size-1;
+    if (selectedServo >= size)
+        selectedServo = size-1;
     
     if (resetMode)
     {
         reset_array(resetModeOut, size);
-        resetModeOut[int(active[0])] = 1;
+        resetModeOut[selectedServo] = 1;
+        
+        if (set[0] == 1)
+        {
+            printf("\n\nDynamixel with ID %i will be restored to factory settings\n\n", servo_id[int(active[0])]);
+            com->Reset(servo_id[int(active[0])],servo[int(active[0])]->protocol);
+            
+            // quit ikaros
+            Notify(msg_terminate, "DynamixelConfigure: Settings written to dynamixel(s). Quiting Ikaros...");
+        }
     }
     else
     {
         reset_array(changeModeOut, size);
-        changeModeOut[int(active[0])] = 1;
-    }
-    
-    // Checking that all inputs are the same.
-    if (resetMode && set[0] == 1)
-    {
+        changeModeOut[selectedServo] = 1;
         
-        printf("\n\nDynamixel with ID %i will be restored to factory settings\n\n", servo_id[int(active[0])]);
-        com->Reset(servo_id[int(active[0])],servo[int(active[0])]->Protocol);
+        PrintChange(selectedServo);
         
-        
-        // quit ikaros
-        Notify(msg_terminate, "DynamixelConfigure: Settings written to dynamixel(s). Quiting Ikaros...");
-        return;
-    }
-    
-    PrintChange(int(active[0]));
-    
-    if(set[0] == 1)
-    {
-        
-        // Write to a single dynamixel ID
-        int i = int(active[0]);
-        
-        // Adding changes
-        if(servo[i])
+        if(set[0] == 1) // Write to a single dynamixel ID
         {
-            servo[i]->SetValueAtAdress(changeAdress, newValue);
-            com->SyncWriteWithIdRange(servo_id, servo[0]->Protocol, DynamixelMemoeries, changeAdress, changeAdress+ servo[i]->ctable[changeAdress].Size-1, size);
+            // Creating a size array
+            int * parameterSize = new int [size];
+            
+            for(int i=0; i<size; i++)
+                parameterSize[i] =  servo[servo_index[i]]->controlTable[changeAdress].Size;
+            
+            // Adding changes
+            servo[selectedServo]->SetValueAtAdress(changeAdress, newValue);
+            
+            // create a temp adress array
+            int * adress = new int [size];
+
+            for (int j = 0; j < size; j++) {
+                adress[j] = changeAdress;
+            }
+            
+            com->WriteToServo(servo_id, protocol, DynamixelMemoeries, adress, parameterSize, size);
             
             timer.Sleep(500); // to make sure we have time to write changes before shuting down ikaros.
             // Shutdown ikaros.
             Notify(msg_terminate, "DynamixelConfigure: Settings written to dynamixel(s). Quiting Ikaros...");
             
+            delete[](adress);
+            delete[](parameterSize);
         }
     }
     
-    // Blink active servo
-    for(int i=0; i<size; i++)
-        if(servo[i])
-            servo[i]->SetValueAtAdress(ikarosInBind[IK_IN_LED], 0);
-    if(servo[int(active[0])])
-        servo[int(active[0])]->SetValueAtAdress(ikarosInBind[IK_IN_LED], blink);
+    // Blink
+    for(int i=0; i<servos; i++)
+        servo[i]->SetValueAtAdress(ikarosInBind[IK_IN_LED][i], 0); // Lights off on all servos
+    servo[selectedServo]->SetValueAtAdress(ikarosInBind[IK_IN_LED][selectedServo], blink); // Lights on on selected servo
     
-    com->SyncWriteWithIdRange(servo_id, servo[0]->Protocol, DynamixelMemoeries, ikarosInBind[IK_IN_LED], ikarosInBind[IK_IN_LED], size);
+    com->WriteToServo(servo_id, protocol, DynamixelMemoeries, ikarosInBind[IK_IN_LED], parameterInSize[IK_IN_LED], size); // write to all servos
+    
     if (blink == 1)
         blink = 0;
     else
         blink = 1;
-    timer.Sleep(100);  // Sleep to get the blinking effect
-    // Blink active servo end
-    
+    timer.Sleep(100);
+    // Blink end
+
+}
+
+void
+DynamixelConfigure::PrintAll()
+{
+    printf("\nDYNAMIXEL\n");
+    printf("Number of servos: %d\n\n", servos);
+    printf("Control table:\n");
+    printf("%-8s %-32s|", "Adress", "Name");
+    for(int i=0; i<servos; i++)
+        printf(" %6s|",servo[servo_index[i]]->extraInfo.Get("Servo Model String"));
+    printf("\n");
+    for (int j = 0; j < 100; j++)
+    {
+        if (servo[servo_index[0]]->controlTable[j].Visable) // Use the description from the first servo
+        {
+            printf("%-8i %-32s|", j, servo[servo_index[0]]->controlTable[j].Name.c_str());
+            for(int i=0; i<servos; i++)
+                printf(" %6i|",servo[servo_index[i]]->GetValueAtAdress(j));
+            printf("\n");
+        }
+    }
 }
 
 static InitClass init("DynamixelConfigure", &DynamixelConfigure::Create, "Source/Modules/RobotModules/Dynamixel/");
-
