d3a4e92dd2021f9c77725a92851cf13e88ec5d4e
ikaros-project <christian@ikaros-project.org>
2016-10-09 15:58:09 +0200

    EventTrigger - final version

---
+++ Source/Modules/UtilityModules/EventTrigger/EventTrigger.h
@@ -0,0 +1,56 @@
+//
+//    EventTrigger.h			This file is a part of the IKAROS project
+//
+//    Copyright (C) 2004  Christian Balkenius
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+
+#ifndef EventTrigger_
+#define EventTrigger_
+
+#include "IKAROS.h"
+
+
+class EventTrigger: public Module
+{
+public:
+
+    EventTrigger(Parameter * p) : Module(p) {}
+    virtual ~EventTrigger() {}
+
+    static Module * Create(Parameter * p) { return new EventTrigger(p); }
+
+    void        SelectNewState();
+
+    void		Init();
+    void		Tick();
+
+    int *       duration;
+    int *       timeout;
+
+    int         size;
+    int         next_size;
+
+    float *     next;
+    float *     output;
+    
+    int         last_state;
+    int         current_state;
+    int         counter;
+};
+
+
+#endif
