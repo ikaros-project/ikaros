d1e15b92b2f3c68c85f3d18f0ad054a2fe08eb90
Christian Balkenius <christian@ikaros-project.org>
2016-11-17 17:01:21 +0100

    removed date

--- Source/Modules/UtilityModules/Flip/Flip.cc
+++ Source/Modules/UtilityModules/Flip/Flip.cc
@@ -17,8 +17,6 @@
 //    along with this program; if not, write to the Free Software
 //    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //
-//	Created: 2004-03-22
-//
 
 
 #include "Flip.h"
