be6348c27d3f15e745e48c691af85d865f33838c
ikaros-project <christian@ikaros-project.org>
2018-01-25 09:08:49 +0100

    WebUI 2.0 Better grid layout

--- Source/WebUI_2.0/WebUIWidgetBarGraph.js
+++ Source/WebUI_2.0/WebUIWidgetBarGraph.js
@@ -10,6 +10,7 @@ class WebUIWidgetBarGraph extends WebUIWidgetCanvas
 */
 
 
+
     static template()
     {
         return [
@@ -24,6 +25,9 @@ class WebUIWidgetBarGraph extends WebUIWidgetCanvas
         this.data = [10, 83, 32, 56, 2, 7, 55];   // Should connect to main data structure
         
         this.onclick = function () { alert(this.data) };
+        
+        this.min = 0;
+        this.max = 1;
     }
 
 
@@ -35,7 +39,7 @@ class WebUIWidgetBarGraph extends WebUIWidgetCanvas
 
 
 
-    update(data)
+    update(data)    // TODO: Move to canvas only?
     {
         let width = parseInt(getComputedStyle(this.canvasElement).width);
         let height = parseInt(getComputedStyle(this.canvasElement).height);
@@ -49,79 +53,252 @@ class WebUIWidgetBarGraph extends WebUIWidgetCanvas
         this.height = parseInt(height);
         
         this.format.width = this.width - this.format.marginLeft - this.format.marginRight;
-        this.format.height = this.height - this.format.marginTop - this.format.marginBottom;
-
+        this.format.height = this.height - this.format.titleHeight - this.format.marginBottom;
 
         this.draw(data);
     }
 
+    // TODO: General functions to draw vertical and horizontal gridlines in rectangle
+    
+    draw_HorizontalGridlines(top, bottom)
+    {
+        let n = this.format.horizontalGridlines;
+        if(n==0)
+            return;
+        
+        let i=top+this.format.marginTop;
+        for(let j=0; j<n; j++)
+        {
+            this.canvas.beginPath();
+            this.canvas.strokeStyle = this.format.gridColor;
+            this.canvas.moveTo(-1, i);
+            this.canvas.lineTo(this.format.width, i);
+            this.canvas.stroke();
+            
+            i += (bottom-(top+this.format.marginTop))/(n-1);
+        }
+    }
 
+    draw_leftTickMarks(top, bottom)
+    {
+        let n = this.format.leftTickMarks;
+        if(n==0)
+            return;
+
+        let i=top+this.format.marginTop;
+        for(let j=0; j<n; j++)
+        {
+            this.canvas.beginPath();
+            this.canvas.lineWidth = 1;
+            this.canvas.strokeStyle = this.format.axisColor;    // maybe also have axis properties
+            this.canvas.moveTo(-1, i);
+            this.canvas.lineTo(-7, i);
+            this.canvas.stroke();
+            
+            i += (bottom-(top+this.format.marginTop))/(n-1);
+        }
+    }
+
+    draw_rightTickMarks(top, bottom)
+    {
+        let n = this.format.rightTickMarks;
+        if(n==0)
+            return;
+
+        let i=top+this.format.marginTop;
+        for(let j=0; j<n; j++)
+        {
+            this.canvas.beginPath();
+            this.canvas.lineWidth = 1;
+            this.canvas.strokeStyle = this.format.axisColor;
+            this.canvas.moveTo(this.format.width, i);
+            this.canvas.lineTo(this.format.width+7, i);
+            this.canvas.stroke();
+            
+            i += (bottom-(top+this.format.marginTop))/(n-1);
+        }
+    }
+
+   // TODO: General functions to draw vertical and horizontal scales outsida a rectangle
+
+    draw_leftScale(top, bottom) // TODO: add heuristics for number of decimals based on max value
+    {
+        let n = this.format.leftScale
+        if(n==0)
+            return;
+
+        this.canvas.font = this.format.scaleFont;
+        this.canvas.fillStyle = this.format.axisColor;
+        this.canvas.textAlign = "right";
+        this.canvas.textBaseline="middle";
+
+        let i=top+this.format.marginTop;
+        for(let j=0; j<n; j++)
+        {
+            let v = this.min + (n-j-1)*(this.max-this.min)/(n-1);
+            this.canvas.fillText(v, -this.format.scaleOffset, i);
+            i += (bottom-(top+this.format.marginTop))/(n-1);
+        }
+        this.canvas.textBaseline="bottom";
+    }
+
+    draw_rightScale(top, bottom) // TODO: add heuristics for number of decimals based on max value
+    {
+        let n = this.format.rightScale
+        if(n==0)
+            return;
+
+        this.canvas.font = this.format.scaleFont;
+        this.canvas.fillStyle = this.format.axisColor;
+        this.canvas.textAlign = "left";
+        this.canvas.textBaseline="middle";
+
+        let i=top+this.format.marginTop;
+        for(let j=0; j<n; j++)
+        {
+            let v = this.min + (n-j-1)*(this.max-this.min)/(n-1);
+            this.canvas.fillText(v, this.format.width+this.format.scaleOffset, i);
+            i += (bottom-(top+this.format.marginTop))/(n-1);
+        }
+        this.canvas.textBaseline="bottom";
+    }
+
+    draw_xAxis(top, bottom)
+    {
+        if(!this.format.xAxis)
+            return;
+
+        this.canvas.beginPath();
+        this.canvas.lineWidth = 1;
+        this.canvas.strokeStyle = this.format.axisColor;
+        this.canvas.moveTo(-1, bottom);
+        this.canvas.lineTo(this.format.width, bottom);
+        this.canvas.stroke();
+    }
+
+    draw_yAxis(top, bottom)
+    {
+        if(!this.format.yAxis)
+            return;
+
+        this.canvas.beginPath();
+        this.canvas.lineWidth = 1;
+        this.canvas.strokeStyle = this.format.axisColor;
+        this.canvas.moveTo(-1, bottom);
+        this.canvas.lineTo(-1, top+this.format.marginTop);
+        this.canvas.stroke();
+    }
+
+    draw_gridFill(top, bottom)
+    {
+        if(this.format.gridFill=="none")
+            return;
+
+        this.canvas.beginPath();
+        this.canvas.fillStyle = this.format.gridFill;
+        this.canvas.rect(-1, this.format.marginTop-1, this.format.width+1, bottom-top);
+        this.canvas.fill();
+    }
+
+    draw_frame(top, bottom)
+    {
+        if(!this.format.frame=="none")
+            return;
+
+        this.canvas.beginPath();
+        this.canvas.strokeStyle = this.format.frame;
+        this.canvas.rect(-1, this.format.marginTop-1, this.format.width+1, this.format.height-this.format.marginTop+1);
+        this.canvas.stroke();
+    }
+
+    // TODO: Function to draw bars in rectangle
 
     draw(d)
     {
-        this.canvas.setTransform(1, 0, 0, 1, 0.5, 0.5);
         this.canvas.clearRect(0, 0, this.width, this.height);
-        this.drawTitle();
+//        this.drawTitle();
+        
+        this.drawFullLayout(1, 7);
+        
+        return;
 
-        this.drawLayout();
+//        this.drawLayout();
+
+        // Vertical Bars
 
         try {
             let m = this.parameters['module']
             let s = this.parameters['source']
             this.data = d[m][s]
-            
+
             if(!this.data)
                 return;
 
             let size_y = this.data.length;
             let size_x = this.data[0].length;
             let pane_y = (this.format.height+1)/size_y;
-            
-            this.canvas.setTransform(1, 0, 0, 1, this.format.marginLeft+0.5, this.format.marginTop+0.5);
-            
+            let n = size_x;
+            let bar_spacing = Math.round(this.format.width/n);
+
+            this.canvas.setTransform(1, 0, 0, 1, this.format.marginLeft+0.5, this.format.titleHeight+0.5);
+
             for(let y=0; y<size_y; y++)
             {
-                this.canvas.beginPath();
-                this.canvas.strokeStyle = "red";
-                this.canvas.rect(-1, pane_y*y-1, this.format.width+1, pane_y);
-                this.canvas.stroke();
+                this.draw_gridFill(pane_y*y-1, pane_y*(y+1)-1);
+                this.draw_HorizontalGridlines(pane_y*y-1, pane_y*(y+1)-1)
+                this.draw_leftScale(pane_y*y-1, pane_y*(y+1)-1)
+                this.draw_rightScale(pane_y*y-1, pane_y*(y+1)-1)
+            }
 
-                let n = size_x;
-                let bar_width = Math.round(this.format.width/ n);
-                let bar_height = pane_y;
+            for(let y=0; y<size_y; y++)
+            {
+                let bar_offset = Math.round(bar_spacing*this.format.spacing/2);
+                let bar_width = Math.round((1-this.format.spacing)*this.format.width/n);
+                let bar_height = pane_y-this.format.marginTop;
                 let max = 1;
-            
+
                 for(let i=0; i<n; i++)
                 {
                     let h = (this.data[y][i]/max)
                     this.canvas.beginPath();
                     this.setColor(i)
-                    this.canvas.rect(i*bar_width, pane_y*(1+y)-1 - bar_height*h, bar_width, bar_height*h);
+                    this.canvas.rect(i*bar_spacing+bar_offset-1, pane_y*(1+y)-1 - bar_height*h, bar_width, bar_height*h);
                     this.canvas.fill();
                     this.canvas.stroke();
                 }
             }
             
-            return;
-            
-            let n = this.data.length;
-            let bar_width = Math.round(this.format.width/ n);
-            let bar_height = this.format.height;
-            let max = 1;
-        
+           for(let y=0; y<size_y; y++)
+            {
+                this.draw_xAxis(pane_y*y-1, pane_y*(y+1)-1);
+                this.draw_yAxis(pane_y*y-1, pane_y*(y+1)-1);
+                this.draw_leftTickMarks(pane_y*y-1, pane_y*(y+1)-1);
+                this.draw_rightTickMarks(pane_y*y-1, pane_y*(y+1)-1);
+                this.draw_frame(pane_y*y-1, pane_y*(y+1)-1);
+           }
+           
             for(let i=0; i<n; i++)
             {
-                let h = (this.data[i]/max)
-                this.canvas.beginPath();
-                this.setColor(i)
-                this.canvas.rect(i*bar_width, bar_height-bar_height*h, bar_width, bar_height*h);
-                this.canvas.fill();
-                this.canvas.stroke();
+ //                this.canvas.rect(i*bar_spacing+bar_offset-1, pane_y*(1+y)-1 - bar_height*h, bar_width, bar_height*h);
+
+                this.canvas.fillStyle = "white"; // this.format.gridColor;
+                this.canvas.textAlign = "center";
+                this.canvas.textBaseline= "bottom";
+
+                let l = this.parameters.labels.split(',');
+                for(let j=0; j<n; j++)
+                {
+                    this.canvas.fillText(l[j].trim(), (j+0.5)*bar_spacing, this.format.height+24);
+                 }
             }
         }
         catch(err)
         {
         }
+
+        // Horizontal Bars
+
+
     }
 };
 
--- Source/WebUI_2.0/WebUIWidgetCanvas.js
+++ Source/WebUI_2.0/WebUIWidgetCanvas.js
@@ -24,35 +24,123 @@ class WebUIWidgetCanvas extends WebUIWidget
         `;
     }
 
-    connectedCallback()
+    getProp(attribute, index)
+    {
+        try
+        {
+            if(index)
+                return getComputedStyle(this.canvasElement).getPropertyValue(attribute).split(",")[index];
+            else
+                return getComputedStyle(this.canvasElement).getPropertyValue(attribute).trim();
+        }
+        catch(err)
+        {
+            return undefined;
+        }
+    }
+
+    getInt(attribute, index)
+    {
+        try
+        {
+            if(index)
+                return parseInt(this.getProp(attribute).split(",")[index]);
+            else
+                return parseInt(this.getProp(attribute));
+        }
+        catch(err)
+        {
+            return 0;
+        }
+    }
+
+    getFloat(attribute, index)
+    {
+        try
+        {
+            if(index)
+                return parseFloat(this.getProp(attribute).split(",")[index]);
+            else
+                return parseFloat(this.getProp(attribute));
+        }
+        catch(err)
+        {
+            return 0;
+        }
+    }
+
+    getBool(attribute, index)
+    {
+        try
+        {
+            if(index)
+                return ['yes','true'].includes(this.getProp(attribute).split(",")[index].toLowerCase());
+            else
+                return ['yes','true'].includes(this.getProp(attribute).toLowerCase());
+        }
+        catch(err)
+        {
+            return false;
+        }
+    }
+
+   connectedCallback()
     {
         this.innerHTML = this.constructor.html();
         this.canvasElement = this.querySelector('canvas');
         this.canvas = this.canvasElement.getContext("2d");
 
-        // read CSS variables
-        
-        this.format = {}
-        
-        this.format.marginTop = parseInt(getComputedStyle(this.canvasElement).getPropertyValue('--margin-top'));
-        this.format.marginBottom = parseInt(getComputedStyle(this.canvasElement).getPropertyValue('--margin-bottom'));
-        this.format.marginLeft = parseInt(getComputedStyle(this.canvasElement).getPropertyValue('--margin-left'));
-        this.format.marginRight = parseInt(getComputedStyle(this.canvasElement).getPropertyValue('--margin-right'));
+        // read CSS variables - TODO: allow to work also when some variable is missing
 
-        this.format.titleFont = getComputedStyle(this.canvasElement).getPropertyValue('--title-font').trim();
-        this.format.titleColor = getComputedStyle(this.canvasElement).getPropertyValue('--title-color').trim();
-        this.format.titleBackground = getComputedStyle(this.canvasElement).getPropertyValue('--title-background').trim();
-        this.format.titleMargins = getComputedStyle(this.canvasElement).getPropertyValue('--title-magins').trim() == "yes";
-        this.format.titleAlign = getComputedStyle(this.canvasElement).getPropertyValue('--title-align').trim();
-        this.format.titleOffset = getComputedStyle(this.canvasElement).getPropertyValue('--title-offset');
-        this.format.titleOffsetX = parseInt(this.format.titleOffset.split(",")[0].trim());
-        this.format.titleOffsetY = parseInt(this.format.titleOffset.split(",")[1].trim());
-        this.format.color = getComputedStyle(this.canvasElement).getPropertyValue('--color').trim();
-        this.format.lineWidth = getComputedStyle(this.canvasElement).getPropertyValue('--line-width').trim();
-        this.format.fill = getComputedStyle(this.canvasElement).getPropertyValue('--fill').trim();
+        this.format = {}
+
+        this.format.titleHeight =           this.getInt('--title-height');
+        this.format.titleFont =             this.getProp('--title-font');
+        this.format.titleColor =            this.getProp('--title-color');
+        this.format.titleBackground =       this.getProp('--title-background');
+        this.format.titleMargins =          this.getBool('--title-margins');
+        this.format.titleAlign =            this.getProp('--title-align');
+        this.format.titleOffsetX =          this.getFloat('--title-offset', 0);
+        this.format.titleOffsetY =          this.getFloat('--title-offset', 1);
+
+        this.format.marginLeft =            this.getInt('--margin-left');
+        this.format.marginRight =           this.getInt('--margin-right');
+        this.format.marginTop =             this.getInt('--margin-top');
+        this.format.marginBottom =          this.getInt('--margin-bottom');
+
+        this.format.spaceLeft =             this.getInt('--space-left');
+        this.format.spaceRight =            this.getInt('--space-right');
+        this.format.spaceTop =              this.getInt('--space-top');
+        this.format.spaceBottom =           this.getInt('--space-bottom');
+
+       this.format.spacing =                this.getFloat('--spacing');
+
+        this.format.color =                 this.getProp('--color');
+        this.format.lineWidth =             this.getProp('--line-width');
+        this.format.fill =                  this.getProp('--fill');
+
+        this.format.gridColor =             this.getProp('--grid-color');
+        this.format.gridLineWidth =         this.getProp('--grid-line-width');
+        this.format.gridFill =              this.getProp('--grid-fill');
+
+        this.format.frame =                 this.getProp('--frame');
+        this.format.xAxis =                 this.getBool('--x-axis');
+        this.format.yAxis =                 this.getBool('--y-axis');
+        this.format.axisColor =             this.getProp('--axis-color');
+        this.format.verticalGridlines =     this.getInt('--vertical-gridlines');
+        this.format.horizontalGridlines =   this.getInt('--horizontal-gridlines');
+        this.format.leftTickMarks =         this.getInt('--left-tick-marks');
+        this.format.rightTickMarks =        this.getInt('--right-tick-marks');
+        this.format.leftScale =             this.getInt('--left-scale');
+        this.format.rightScale =            this.getInt('--right-scale');
+        this.format.scaleOffset =           this.getInt('--scale-offset');
+        this.format.scaleFont =             this.getProp('--scale-font');
+        this.format.labels =                this.getBool('--labels');
+
+        this.format.direction =             this.getProp('--direction');
 
         // read parameters
-        
+
         // set colors etc from parameters if set
     }
 
@@ -76,35 +164,37 @@ class WebUIWidgetCanvas extends WebUIWidget
         this.canvas.beginPath();
         this.canvas.fillStyle = this.format.titleBackground;
         if(this.format.titleBackgroundMargins)
-            this.canvas.rect(+this.format.marginLeft-0.5, 0, this.width - this.format.marginRight-0.5, +this.format.marginTop-0.5);
+            this.canvas.rect(+this.format.marginLeft-0.5, 0, this.width - this.format.marginRight-0.5, +this.format.titleHeight-0.5);
         else
-            this.canvas.rect(-0.5, -0.5, this.width, +this.format.marginTop);
+            this.canvas.rect(-0.5, -0.5, this.width, +this.format.titleHeight);
         this.canvas.fill();
 
         this.canvas.font = this.format.titleFont;
         this.canvas.fillStyle = this.format.titleColor;
         this.canvas.textAlign = this.format.titleAlign;
+        this.canvas.textBaseline="bottom";
 
         if(this.format.titleMargins)
         {
             if(this.canvas.textAlign == 'left')
-                this.canvas.fillText("Title", this.format.marginLeft+this.format.titleOffsetX, this.format.marginTop+this.format.titleOffsetY-1);
+                this.canvas.fillText(this.parameters.title, this.format.marginLeft+this.format.titleOffsetX, this.format.titleHeight+this.format.titleOffsetY-1);
             else if(this.canvas.textAlign == 'right')
-                this.canvas.fillText("Title", this.width - this.format.marginRight-this.format.titleOf. this.format.marginTop+this.format.titleOffsetY-1);
+                this.canvas.fillText(this.parameters.title, this.width - this.format.marginRight-this.format.titleOf. this.format.titleHeight+this.format.titleOffsetY-1);
             else if(this.canvas.textAlign == 'center')
-                this.canvas.fillText("Title", +this.format.marginLeft+this.format.width/2, +this.format.marginTop+this.format.titleOffsetY-1);
+                this.canvas.fillText(this.parameters.title, +this.format.marginLeft+this.format.width/2, +this.format.titleHeight+this.format.titleOffsetY-1);
         }
         else
         {
              if(this.canvas.textAlign == 'left')
-                this.canvas.fillText("OTitleq", this.format.titleOffsetX-1, this.format.marginTop+this.format.titleOffsetY-1);
+                this.canvas.fillText(this.parameters.title, this.format.titleOffsetX-1, this.format.titleHeight+this.format.titleOffsetY-1);
             else if(this.canvas.textAlign == 'right')
-                this.canvas.fillText("Title", this.width-this.format.titleOffsetX, this.format.marginTop+this.format.titleOffsetY-1);
+                this.canvas.fillText(this.parameters.title, this.width-this.format.titleOffsetX, this.format.titleHeight+this.format.titleOffsetY-1);
             else if(this.canvas.textAlign == 'center')
-                this.canvas.fillText("Title", this.width/2, this.format.marginTop+this.format.titleOffsetY-1);
+                this.canvas.fillText(this.parameters.title, this.width/2, this.format.titleHeight+this.format.titleOffsetY-1);
         }
     }
 
+    // DRAW GRID UN RECT H & V
 
     setColor(i)
     {
@@ -124,8 +214,8 @@ class WebUIWidgetCanvas extends WebUIWidget
         this.canvas.lineWidth = 1;
         this.canvas.strokeStyle = "gray";
 
-        this.canvas.moveTo(-0.5, this.format.marginTop-1);
-        this.canvas.lineTo(this.width, this.format.marginTop-1);
+        this.canvas.moveTo(-0.5, this.format.titleHeight-1);
+        this.canvas.lineTo(this.width, this.format.titleHeight-1);
 
         this.canvas.moveTo(-0.5, this.height-this.format.marginBottom);
         this.canvas.lineTo(this.width, this.height-this.format.marginBottom);
@@ -138,7 +228,126 @@ class WebUIWidgetCanvas extends WebUIWidget
 
         this.canvas.stroke();
     }
+
+
+    drawRow(width, height, row)
+    {
     
+    }
+
+    drawColumn()
+    {
+    
+    }
+
+    drawHorizontalPlot()
+    {
+        // draw gridlines
+        // draw row
+    }
+
+    drawVerticalPlot()
+    {
+        // draw gridlines
+        // draw column
+    }
+
+    drawPane()
+    {
+        // draw axes and frames
+    }
+
+    drawHorizontalGridlines(width, height)
+    {
+        let n = this.format.horizontalGridlines;
+        if(n==0)
+            return;
+        
+        let p=0;
+        for(let j=0; j<n; j++)
+        {
+            let q = Math.round(p)
+            this.canvas.beginPath();
+            this.canvas.strokeStyle = this.format.gridColor;
+            this.canvas.moveTo(0, q);
+            this.canvas.lineTo(width, q);
+            this.canvas.stroke();
+            p += height/(n-1);
+        }
+    }
+
+    drawFullLayout(size_x, size_y)
+    {
+        this.canvas.setTransform(1, 0, 0, 1, -0.5, -0.5);
+
+        this.canvas.beginPath();
+        this.canvas.lineWidth = 1;
+        this.canvas.strokeStyle = "gray";
+
+        // margins
+
+        this.canvas.moveTo(0.5, this.format.marginTop-1);
+        this.canvas.lineTo(this.width, this.format.marginTop-1);
+
+        this.canvas.moveTo(0.5, this.height-this.format.marginBottom);
+        this.canvas.lineTo(this.width, this.height-this.format.marginBottom);
+
+        this.canvas.moveTo(this.format.marginLeft-1, 0.5);
+        this.canvas.lineTo(this.format.marginLeft-1, this.height);
+
+        this.canvas.moveTo(this.width-this.format.marginRight, 0.5);
+        this.canvas.lineTo(this.width-this.format.marginRight, this.height);
+
+        this.canvas.stroke();
+
+        // boxes
+        
+        let pane_y = (this.height - this.format.marginTop - this.format.marginBottom+1)/size_y;
+        let pane_x = (this.width - this.format.marginLeft - this.format.marginRight+1)/size_x;
+
+
+        for(let y=0; y<size_y; y++)
+        {
+            let top = Math.round(y*(pane_y));
+            let bottom = Math.round((y+1)*pane_y);
+            
+            this.canvas.setTransform(1, 0, 0, 1, this.format.marginLeft-1.5, top+this.format.marginTop-1.5);
+            this.canvas.beginPath();
+            this.canvas.lineWidth = 1;
+            this.canvas.strokeStyle = "red";
+            this.canvas.rect(0, 0, this.format.width+1, bottom-top);
+            this.canvas.stroke();
+
+            // axes
+            this.canvas.setTransform(1, 0, 0, 1, this.format.marginLeft-1.5, top+this.format.marginTop+this.format.spaceTop-1.5);
+            this.canvas.beginPath();
+            this.canvas.lineWidth = 1;
+            this.canvas.strokeStyle = "yellow";
+            this.canvas.rect(0, 0, this.format.width+1, bottom-top-this.format.spaceTop);
+            this.canvas.stroke();
+
+            // gridlines, ticks and scales
+            this.canvas.setTransform(1, 0, 0, 1, this.format.marginLeft-1.5, this.format.marginTop+top+this.format.spaceTop-1.5);
+            this.canvas.beginPath();
+            this.canvas.lineWidth = 1;
+            this.canvas.strokeStyle = "blue";
+            this.canvas.rect(0, 0, this.format.width+1, bottom-top-this.format.spaceTop);
+            this.canvas.stroke();
+            
+            this.drawHorizontalGridlines(this.format.width+1, bottom-top-this.format.spaceTop)
+
+            // plot
+            this.canvas.setTransform(1, 0, 0, 1, this.format.marginLeft+this.format.spaceLeft-1.5, this.format.marginTop+top+this.format.spaceTop-1.5);
+            this.canvas.beginPath();
+            this.canvas.lineWidth = 1;
+            this.canvas.strokeStyle = "green";
+            this.canvas.rect(0, 0, this.format.width-this.format.spaceLeft-this.format.spaceRight+1, bottom-top-this.format.spaceTop-this.format.spaceBottom);
+            this.canvas.stroke();
+
+        }
+    }
+
+
     draw()
     {
         this.canvas.beginPath();
