1038cf3acb1003ea056d139e7ceff1019d65e697
Christian Balkenius <christian@ikaros-project.org>
2018-09-14 00:00:17 +0200

    ActionCompetition first commit

---
+++ Source/Modules/RobotModules/ActionCompetition/ActionCompetition.cc
@@ -0,0 +1,71 @@
+//
+//    ActionCompetition.cc		This file is a part of the IKAROS project
+//
+//    Copyright (C) 2018  Christian Balkenius
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+
+
+#include "ActionCompetition.h"
+
+using namespace ikaros;
+
+void
+ActionCompetition::Init()
+{
+    output          = GetOutputArray("OUTPUT");
+    output_size     = GetOutputSize("OUTPUT");
+    
+    input          = GetInputArray("INPUT");
+    input_size     = GetInputSize("INPUT");
+
+    complete        = GetInputArray("COMPLETE");
+    complete_size    = GetInputSize("COMPLETE");
+    
+    counter         = GetIntValue("initial_delay");
+    
+    duration    = GetIntArray("duration", output_size, true);
+
+    rest            = GetArray("rest", output_size, true);
+    min_            = GetArray("min", output_size, true);
+    max_             = GetArray("max", output_size, true);
+    passive         = GetArray("passive", output_size, true);
+    bias            = GetArray("bias", output_size, true);
+    completion_bias  = GetArray("completion_bias", output_size, true);
+}
+
+
+
+void
+ActionCompetition::Tick()
+{
+    for(int i=0; i<output_size; i++)
+    {
+        if(i<input_size)
+            output[i] += input[i]*bias[i];
+
+        if(i<complete_size)
+            output[i] += complete[i]*completion_bias[i];
+
+        output[i] += (rest[i]-output[i])*passive[i]; // rest
+        output[i] = clip(output[i], min_[i], max_[i]);
+    }
+}
+
+
+
+static InitClass init("ActionCompetition", &ActionCompetition::Create, "Source/Modules/RobotModules/ActionCompetition/");
+
---
+++ Source/Modules/RobotModules/ActionCompetition/ActionCompetition.h
@@ -0,0 +1,63 @@
+//
+//    ActionCompetition.h			This file is a part of the IKAROS project
+//
+//    Copyright (C) 2018  Christian Balkenius
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+
+#ifndef ActionCompetition_
+#define ActionCompetition_
+
+#include "IKAROS.h"
+
+
+class ActionCompetition: public Module
+{
+public:
+
+    ActionCompetition(Parameter * p) : Module(p) {}
+    virtual ~ActionCompetition() {}
+
+    static Module * Create(Parameter * p) { return new ActionCompetition(p); }
+
+    void        SelectNewState();
+
+    void        Init();
+    void        Tick();
+
+    int *       duration; // generates completion internally after this time; 0 equals no internal completion
+    int *       timeout;
+
+    float *     input;
+    float *     complete;
+    float *     output;
+
+    float *     rest;
+    float *     min_;
+    float *     max_;
+    float *     passive;
+    float *     bias;
+    float *     completion_bias;
+
+    int         input_size;
+    int         output_size;
+    int         complete_size;
+
+    int         counter;
+};
+
+
+#endif
