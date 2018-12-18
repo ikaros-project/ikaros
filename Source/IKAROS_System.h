9a6e77d079724447591e8ca824dc2630b3c4a39e
ikaros-project <christian@ikaros-project.org>
2018-12-18 22:18:33 +0100

    Removed old defined, JPEG always needed

--- Source/IKAROS_System.h
+++ Source/IKAROS_System.h
@@ -57,13 +57,8 @@
 #define PLATFORM  "LINUX"
 #define POSIX
 #define USE_BSD_SOCKET
-//#define USE_QUICKTIME
-#define USE_LIBJPEG
-//#define USE_V4L2
-//#define USE_KEEPON
 #define USE_BLAS
 #define USE_LIBPNG
-//#define USE_MPI
 #endif
 
 
@@ -77,10 +72,7 @@
 #define POSIX
 #define USE_BSD_SOCKET
 #define USE_QUICKTIME
-//#define USE_QUICKTIME_OLD
-#define USE_LIBJPEG
 #define USE_LIBPNG
-//#define USE_VDSP
 #define USE_VIMAGE
 #define USE_VFORCE
 #define USE_BLAS
@@ -99,13 +91,6 @@
 
 #ifdef MAC_OS_X
 #include <limits.h>
-#endif
-
-
-// Include the socket header file if some socket library is used
-
-#ifdef USE_BSD_SOCKET
-#define USE_SOCKET
 #endif
 
 // Fix for missing MSG_NOSIGNAL in OS X to pass compilation
