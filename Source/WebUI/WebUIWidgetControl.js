c74b1863fc0f73f5acf9f87a5e43173651625fce
ikaros-project <christian@ikaros-project.org>
2019-01-10 15:39:18 +0100

    Controls almost working

--- Source/WebUI/WebUIWidgetControl.js
+++ Source/WebUI/WebUIWidgetControl.js
@@ -1,5 +1,23 @@
 class WebUIWidgetControl extends WebUIWidget
 {
+    // TODO: these should be changed to go through command object to allow faster update
+
+    send_control_change(parameter, value=0, index_x=0, index_y=0)
+    {
+        if(this.groupName)
+            this.get("/control/"+this.groupName+"."+parameter+"/"+index_x+"/"+index_y+"/"+value);
+        else
+            this.get("/control/"+parameter+"/"+index_x+"/"+index_y+"/"+value);
+    }
+
+    send_xcommand(command, value=0, index_x=0, index_y=0)
+    {
+        if(this.groupName)
+            this.get("/command/"+this.groupName+"."+command+"/"+index_x+"/"+index_y+"/"+value);
+        else
+            this.get("/command/"+command+"/"+index_x+"/"+index_y+"/"+value);
+    }
+
 };
 
 webui_widgets.add('webui-widget-control', WebUIWidgetControl);
