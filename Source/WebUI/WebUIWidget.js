083a584b5e95123b667bbf9d32aa58fe161c5d05
ikaros-project <christian@ikaros-project.org>
2019-02-19 01:53:40 +0100

    Control code moved to widget superclass

--- Source/WebUI/WebUIWidget.js
+++ Source/WebUI/WebUIWidget.js
@@ -426,6 +426,25 @@ class WebUIWidget extends HTMLElement
         this.updateFrame();
         this.update();
     }
+    
+    // TODO: these should be changed to go through command object to allow faster update
+
+    send_control_change(parameter, value=0, index_x=0, index_y=0)
+    {
+        if(this.groupName)
+            this.get("/control/"+this.groupName+"."+parameter+"/"+index_x+"/"+index_y+"/"+value);
+        else
+            this.get("/control/"+parameter+"/"+index_x+"/"+index_y+"/"+value);
+    }
+
+    send_command(command, value=0, index_x=0, index_y=0)
+    {
+        if(this.groupName)
+            this.get("/command/"+this.groupName+"."+command+"/"+index_x+"/"+index_y+"/"+value);
+        else
+            this.get("/command/"+command+"/"+index_x+"/"+index_y+"/"+value);
+    }
+
 };
 
 customElements.define('webui-widget', WebUIWidget);
