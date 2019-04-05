aacc1fbea2ec8da3c48ee4bab426ecf2653be230
christianbalkenius <christian.balkenius@lucs.lu.se>
2019-04-05 17:50:57 +0200

    Moren2002 updated

--- Source/Modules/BrainModels/Moren2002/M02_Cortex/M02_Cortex.cc
+++ Source/Modules/BrainModels/Moren2002/M02_Cortex/M02_Cortex.cc
@@ -58,4 +58,4 @@ M02_Cortex::Tick()
 
 
 
-static InitClass init("M02_Cortex", &M02_Cortex::Create, "Source/Modules/BrainModels/M02_Cortex/");
+static InitClass init("M02_Cortex", &M02_Cortex::Create, "Source/Modules/BrainModels/Moren2002/M02_Cortex/");
