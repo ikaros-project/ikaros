a5e2ce6fa029ec1c15fb7894c15904c607422d5b
Birger Johansson <m@birgerjohansson.com>
2020-03-02 20:47:23 +0100

    Update EpiHead to better fit Epi
    Also added a direct connection from motionrecorder. This should be removed in the future.

--- Source/WebUI/WebUIWidgetEpiHead.js
+++ Source/WebUI/WebUIWidgetEpiHead.js
@@ -1,60 +1,54 @@
-class WebUIWidgetEpiHead extends WebUIWidgetGraph
-{
-    static template()
-    {
+class WebUIWidgetEpiHead extends WebUIWidgetGraph {
+    static template() {
         return [
-            {'name': "EPI HEAD", 'control':'header'},
-            {'name':'title', 'default':"Epi Head", 'type':'string', 'control': 'textedit'},
-            {'name':'gazeSource', 'default':"", 'type':'source', 'control': 'textedit'},
-            {'name':'pupilLeftSource', 'default':"", 'type':'source', 'control': 'textedit'},
-            {'name':'pupilRightSource', 'default':"", 'type':'source', 'control': 'textedit'},
-            {'name':'headPosition', 'default':"", 'type':'source', 'control': 'textedit'},
+            { 'name': "EPI HEAD", 'control': 'header' },
+            { 'name': 'title', 'default': "Epi Head", 'type': 'string', 'control': 'textedit' },
+            { 'name': 'gazeSource', 'default': "", 'type': 'source', 'control': 'textedit' },
+            { 'name': 'pupilLeftSource', 'default': "", 'type': 'source', 'control': 'textedit' },
+            { 'name': 'pupilRightSource', 'default': "", 'type': 'source', 'control': 'textedit' },
+            { 'name': 'headPosition', 'default': "", 'type': 'source', 'control': 'textedit' },
 
-            {'name':'irisLeftRed', 'default':"", 'type':'source', 'control': 'textedit'},
-            {'name':'irisLeftGreen', 'default':"", 'type':'source', 'control': 'textedit'},
-            {'name':'irisLeftBlue', 'default':"", 'type':'source', 'control': 'textedit'},
-            
-            {'name':'irisRightRed', 'default':"", 'type':'source', 'control': 'textedit'},
-            {'name':'irisRightGreen', 'default':"", 'type':'source', 'control': 'textedit'},
-            {'name':'irisRightBlue', 'default':"", 'type':'source', 'control': 'textedit'},
-            
-            {'name':'mouthRed', 'default':"", 'type':'source', 'control': 'textedit'},
-            {'name':'mouthGreen', 'default':"", 'type':'source', 'control': 'textedit'},
-            {'name':'mouthBlue', 'default':"", 'type':'source', 'control': 'textedit'},
-            
-            {'name':'gaze', 'default':0, 'type':'float', 'control': 'slider', 'min': -1.57, 'max': 1.57},
-            {'name':'vergence', 'default':0, 'type':'float', 'control': 'slider', 'min': -1.57, 'max': 1.57},
-            {'name':'pupil', 'default':0.5, 'type':'float', 'control': 'slider', 'min': 0, 'max': 1},
-            
-            {'name':'visibleSource', 'default':"", 'type':'source', 'control': 'textedit'},
-            {'name':'visibleFace', 'default':true, 'type':'bool', 'control': 'checkbox'},
-            {'name':'visibleFaceParameter', 'default':"", 'type':'source', 'control': 'textedit'},
-                
-            {'name': "STYLE", 'control':'header'},
+            { 'name': 'irisLeftRGB', 'default': "", 'type': 'source', 'control': 'textedit' },
+            { 'name': 'irisRightRGB', 'default': "", 'type': 'source', 'control': 'textedit' },
+            { 'name': 'mouthRGB', 'default': "", 'type': 'source', 'control': 'textedit' },
 
-            {'name':'color', 'default':"black", 'type':'string', 'control': 'textedit'},
-            {'name':'fill', 'default':"white", 'type':'string', 'control': 'textedit'},
-            {'name':'earColor', 'default':"#0088ff", 'type':'string', 'control': 'textedit'},
-            {'name':'irisColor', 'default':"#88aaff", 'type':'string', 'control': 'textedit'},
-            {'name':'mouthColor', 'default':"#000000", 'type':'string', 'control': 'textedit'},
-            {'name':'show_title', 'default':false, 'type':'bool', 'control': 'checkbox'},
-            {'name':'show_frame', 'default':false, 'type':'bool', 'control': 'checkbox'},
-            {'name':'style', 'default':"", 'type':'string', 'control': 'textedit'},
-            {'name':'frame-style', 'default':"", 'type':'string', 'control': 'textedit'}
+            { 'name': 'irisLeftIntensity', 'default': "", 'type': 'source', 'control': 'textedit' },
+            { 'name': 'irisRightIntensity', 'default': "", 'type': 'source', 'control': 'textedit' },
+            { 'name': 'mouthIntensity', 'default': "", 'type': 'source', 'control': 'textedit' },
+
+            { 'name': 'motionRecorderInput', 'default': "", 'type': 'source', 'control': 'textedit' },
+
+            { 'name': 'gaze', 'default': 0, 'type': 'float', 'control': 'slider', 'min': -45, 'max': 45 },
+            { 'name': 'vergence', 'default': 0, 'type': 'float', 'control': 'slider', 'min': -20, 'max': 20 },
+            { 'name': 'pupilInMM', 'default': 11, 'type': 'float', 'control': 'slider', 'min': 6, 'max': 16 },
+
+            { 'name': 'visibleSource', 'default': "", 'type': 'source', 'control': 'textedit' },
+            { 'name': 'visibleFace', 'default': true, 'type': 'bool', 'control': 'checkbox' },
+            { 'name': 'visibleFaceParameter', 'default': "", 'type': 'source', 'control': 'textedit' },
+
+            { 'name': "STYLE", 'control': 'header' },
+
+            { 'name': 'color', 'default': "black", 'type': 'string', 'control': 'textedit' },
+            { 'name': 'fill', 'default': "white", 'type': 'string', 'control': 'textedit' },
+            { 'name': 'earColor', 'default': "#0088ff", 'type': 'string', 'control': 'textedit' },
+            { 'name': 'irisColor', 'default': "#88aaff", 'type': 'string', 'control': 'textedit' },
+            { 'name': 'mouthColor', 'default': "#000000", 'type': 'string', 'control': 'textedit' },
+            { 'name': 'show_title', 'default': false, 'type': 'bool', 'control': 'checkbox' },
+            { 'name': 'show_frame', 'default': false, 'type': 'bool', 'control': 'checkbox' },
+            { 'name': 'style', 'default': "", 'type': 'string', 'control': 'textedit' },
+            { 'name': 'frame-style', 'default': "", 'type': 'string', 'control': 'textedit' }
         ]
     };
 
 
-    init()
-    {
+    init() {
         super.init();
 
         this.onclick = function () { alert(this.data) }; // last matrix
     }
 
 
-    drawEye(x, y, gaze, pupil, red, green, blue)
-    {
+    drawEye(x, y, gaze, pupil, rgb, alpha) {
         this.canvas.save();
 
         // Eye outline
@@ -74,36 +68,37 @@ class WebUIWidgetEpiHead extends WebUIWidgetGraph
         this.canvas.closePath();
         this.canvas.fill();
         this.canvas.clip();
-        
-        let v = 27.5*Math.sin(gaze)
-        
+
+        // Should be matched with Epi!
+        let v = 27.5 * Math.sin(gaze * Math.PI / 180)
+
         // Iris
-        
-        if(red && green && blue)
+        if (rgb)
             this.canvas.fillStyle = 'rgb('
-                + Math.floor(255*red[0][0]) + ','
-                + Math.floor(255*green[0][0]) + ','
-                + Math.floor(255*blue[0][0]) + ')'
+                + Math.floor(255 * rgb[0]) + ','
+                + Math.floor(255 * rgb[1]) + ','
+                + Math.floor(255 * rgb[2]) + ','
+                + alpha + ')'
+
         else
             this.canvas.fillStyle = this.parameters.irisColor; // when LED input not connected
-        
-        
+
         this.canvas.strokeStyle = "#00000000";
         this.canvas.beginPath();
-        this.canvas.arc(27.5+v, 20, 17.5, 0, 2*Math.PI);
+        this.canvas.arc(27.5 + v, 20, 17.5, 0, 2 * Math.PI);
         this.canvas.fill();
 
         // Pupil
 
         this.canvas.fillStyle = "black";
         this.canvas.beginPath();
-        this.canvas.arc(27.5+v, 20, pupil*17.5, 0, 2*Math.PI);
+        this.canvas.arc(27.5 + v, 20, pupil / 18 * (17.5 * 0.5625), 0, 2 * Math.PI);
         this.canvas.fill();
 
         this.canvas.restore();
-        
+
         // Draw outline again
-        
+
         this.canvas.save();
         this.canvas.translate(x, y);
         this.setColor(0);
@@ -122,26 +117,25 @@ class WebUIWidgetEpiHead extends WebUIWidgetGraph
     }
 
 
-    draw(gaze, pupil, offset, iLR, iLG, iLB, iRR, iRG, iRB)
-    {
+    draw(gaze, pupil, offset, iLRGB, iLI, iRRGB, iRI, mRGB, mI)
+     {
         let w = this.width;
         let h = this.height;
-        let s = Math.min(this.width, this.height)/180;
-        let mw = Math.floor(0.5*(w-s*160))+0.5;
-        let mh = Math.floor(0.5*(h-s*165))+0.5;
+        let s = Math.min(this.width, this.height) / 180;
+        let mw = Math.floor(0.5 * (w - s * 160)) + 0.5;
+        let mh = Math.floor(0.5 * (h - s * 165)) + 0.5;
 
-        this.canvas.clearRect(-1, -1, this.width+1, this.height+1);
+        this.canvas.clearRect(-1, -1, this.width + 1, this.height + 1);
         this.canvas.setTransform(s, 0, 0, s, mw, mh);
         this.canvas.lineWidth = 1;
-        
+
         this.canvas.translate(offset[0], offset[1]);
 
         let drawFace = this.parameters.visibleFace;
-        if(this.parameters.visibleFaceParameter)
+        if (this.parameters.visibleFaceParameter)
             drawFace = !!parseFloat(this.getSource("visibleFaceParameter"));
-        
-        if(drawFace)
-        {
+
+        if (drawFace) {
             // right ear
             this.setColor(0);
             this.canvas.fillStyle = this.parameters.earColor;
@@ -149,7 +143,7 @@ class WebUIWidgetEpiHead extends WebUIWidgetGraph
             this.canvas.rect(5, 60, 10, 65);
             this.canvas.fill();
             this.canvas.stroke();
-  
+
             // right ear lid
             this.setColor(0);
             this.canvas.beginPath();
@@ -160,7 +154,7 @@ class WebUIWidgetEpiHead extends WebUIWidgetGraph
             this.canvas.closePath();
             this.canvas.fill();
             this.canvas.stroke();
-            
+
             // left ear
             this.setColor(0);
             this.canvas.fillStyle = this.parameters.earColor;
@@ -168,7 +162,7 @@ class WebUIWidgetEpiHead extends WebUIWidgetGraph
             this.canvas.rect(145, 60, 10, 65);
             this.canvas.fill();
             this.canvas.stroke();
-  
+
             // left ear lid
             this.setColor(0);
             this.canvas.beginPath();
@@ -179,7 +173,7 @@ class WebUIWidgetEpiHead extends WebUIWidgetGraph
             this.canvas.closePath();
             this.canvas.fill();
             this.canvas.stroke();
-            
+
             // head
             this.setColor(0);
             this.canvas.beginPath();
@@ -212,59 +206,91 @@ class WebUIWidgetEpiHead extends WebUIWidgetGraph
             this.canvas.stroke();
 
             // Mouth
-        
-            this.canvas.fillStyle = this.parameters.mouthColor;
-            for(let c=0; c<2; c++)
-                for(let i=0; i<6; i++)
-                {
+
+
+
+            if (mRGB)
+                this.canvas.fillStyle = 'rgba('
+                    + Math.floor(255 * mRGB[0]) + ','
+                    + Math.floor(255 * mRGB[1]) + ','
+                    + Math.floor(255 * mRGB[2]) + ','
+                    + mI + ')'
+            else
+                this.canvas.fillStyle = this.parameters.mouthColor;
+
+            for (let c = 0; c < 2; c++)
+                for (let i = 0; i < 6; i++) {
                     this.canvas.beginPath();
-                    this.canvas.arc(80-2.5*6.5+i*6.5, 135+c*10, 2, 0, 2*Math.PI);
+                    this.canvas.arc(80 - 2.5 * 6.5 + i * 6.5, 135 + c * 10, 2, 0, 2 * Math.PI);
                     this.canvas.fill();
                     this.canvas.stroke();
                 }
         }
-         // eyes
-        
-        this.drawEye(22.5, 72.5, gaze[0], pupil[0], iLR, iLG, iLB);
-        this.drawEye(85, 72.5, gaze[1], pupil[1], iRR, iRG, iRB);
+
+        // eyes
+        this.drawEye(22.5, 72.5, gaze[0], pupil[0], iLRGB, iLI);
+        this.drawEye(85, 72.5, gaze[1], pupil[1], iRRGB, iRI);
+
     }
 
 
-    update()
-    {
+    update() {
         let v = this.getSource('visibleSource');
-        if(v && v[0][0] == 0)
-        {
-            this.canvas.clearRect(-1, -1, this.width+1, this.height+1);
+        if (v && v[0][0] == 0) {
+            this.canvas.clearRect(-1, -1, this.width + 1, this.height + 1);
             return;
         }
 
-        let defaultGaze = [ parseFloat(this.parameters.gaze)-parseFloat(this.parameters.vergence),
-                            parseFloat(this.parameters.gaze)+parseFloat(this.parameters.vergence)];
-        let defaultPupil = parseFloat(this.parameters.pupil);
-        
-        let gaze = this.getSourceAsArray('gazeSource', defaultGaze);
-        let test = this.getSourceAsFloat('pupilLeftSource', defaultPupil);
-        let pupil = [this.getSourceAsFloat('pupilLeftSource', defaultPupil), this.getSourceAsFloat('pupilRightSource', defaultPupil)];
-        let headPosition = this.getSource('headPosition', [[0, 0]]);
-        
-        let iLR = this.getSource('irisLeftRed');
-        let iLG = this.getSource('irisLeftGreen');
-        let iLB = this.getSource('irisLeftBlue');
-        let iRR = this.getSource('irisRightRed');
-        let iRG = this.getSource('irisRightGreen');
-        let iRB = this.getSource('irisRightBlue');
+        // Special motion recorder input. Should be removed when we have better select in webUI source
+        if (this.getSource('motionRecorderInput')) {
+            let motionRecorderSource = this.getSourceAsArray('motionRecorderInput');
+            let headPosition = [[0, 0]];
+            let gaze = [motionRecorderSource[2], motionRecorderSource[3]];
+            let pupil = [motionRecorderSource[4], motionRecorderSource[5]];
+            let iLRGB = [motionRecorderSource[6], motionRecorderSource[7], motionRecorderSource[8]];
+            let iRRGB = [motionRecorderSource[9], motionRecorderSource[10], motionRecorderSource[11]];
+            let mRGB = [motionRecorderSource[12], motionRecorderSource[13], motionRecorderSource[14]];
 
-        if(gaze.length<2)
-            gaze = [gaze[0], gaze[0]]
-        
-        if(headPosition.length<2)
-            headPosition = [headPosition[0], 0]
-        
-        this.draw(gaze, pupil, headPosition, iLR, iLG, iLB, iRR, iRG, iRB);
+            let iLI = [motionRecorderSource[15]];
+            let iRI = [motionRecorderSource[16]];
+            let mI = [motionRecorderSource[17]];
+
+            if (gaze.length < 2)
+                gaze = [gaze[0], gaze[0]]
+
+            if (headPosition.length < 2)
+                headPosition = [headPosition[0], 0]
+
+            this.draw(gaze, pupil, headPosition, iLRGB, iLI, iRRGB, iRI, mRGB, mI);
+        }
+        else {
+            let defaultGaze = [parseFloat(this.parameters.gaze) - parseFloat(this.parameters.vergence),
+            parseFloat(this.parameters.gaze) + parseFloat(this.parameters.vergence)];
+            let defaultPupil = parseFloat(this.parameters.pupilInMM);
+
+            let gaze = this.getSourceAsArray('gazeSource', defaultGaze);
+            let pupil = [this.getSourceAsFloat('pupilLeftSource', defaultPupil), this.getSourceAsFloat('pupilRightSource', defaultPupil)];
+            let headPosition = this.getSource('headPosition', [[0, 0]]);
+            let iLRGB = this.getSourceAsArray('irisLeftRGB');
+            let iRRGB = this.getSourceAsArray('irisRightRGB');
+            let mRGB = this.getSourceAsArray('mouthRGB');
+
+            let iLI = this.getSourceAsFloat('irisLeftIntensity',[[1]]);
+            let iRI = this.getSourceAsFloat('irisRightIntensity',[[1]]);
+            let mI = this.getSourceAsFloat('mouthIntensity',[[1]]);
+                       
+            if (gaze.length < 2)
+                gaze = [gaze[0], gaze[0]]
+
+            if (headPosition.length < 2)
+                headPosition = [headPosition[0], 0]
+            
+            this.draw(gaze, pupil, headPosition, iLRGB, iLI, iRRGB, iRI, mRGB, mI);
+
+            }
+
     }
 };
-
 
 webui_widgets.add('webui-widget-epi-head', WebUIWidgetEpiHead);
 
