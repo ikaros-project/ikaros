4d7dcd26b02f570d9e69b6ab2524e0080c3b1514
Christian Balkenius <christian.balkenius@lucs.lu.se>
2020-04-25 22:55:25 +0200

    Fixes #134.

--- Source/WebUI/WebUIWidgetMarker.js
+++ Source/WebUI/WebUIWidgetMarker.js
@@ -11,8 +11,8 @@ class WebUIWidgetMarker extends WebUIWidgetGraph
 //            {'name':'length_module', 'default':"", 'type':'source', 'control': 'textedit'},
 //            {'name':'length_source', 'default':"", 'type':'source', 'control': 'textedit'},
             {'name':'order', 'default':"col", 'type':'string', 'control': 'menu', 'values': "col,row"},
-            {'name':'select', 'default':"", 'type':'string', 'control': 'textedit'},
-            {'name':'selectValue', 'default':"", 'type':'string', 'control': 'textedit'},
+            {'name':'select', 'default':0, 'type':'int', 'control': 'textedit'},
+            {'name':'selectValue', 'default':0, 'type':'int', 'control': 'textedit'},
             {'name':'count', 'default':0, 'type':'int', 'control': 'textedit'},
 
             {'name': "MARKER STYLE", 'control':'header'},
