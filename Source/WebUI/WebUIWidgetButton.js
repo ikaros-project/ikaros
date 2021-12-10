13342bc6caee82a5f4478aa5cb31f33af488eae8
Christian Balkenius <christian.balkenius@lucs.lu.se>
2021-12-10 01:29:55 +0100

    Added button functionality
    Push, toggle, radio and icon.

--- Source/WebUI/WebUIWidgetButton.js
+++ Source/WebUI/WebUIWidgetButton.js
@@ -4,13 +4,25 @@ class WebUIWidgetButton extends WebUIWidgetControl
     {
         return [
             {'name': "BUTTON", 'control':'header'},
+
             {'name':'title', 'default':"Title", 'type':'string', 'control': 'textedit'},
             {'name':'label', 'default':"Button", 'type':'string', 'control': 'textedit'},
- 
+
+            {'name': "STYLE", 'control':'header'},
+            {'name':'color', '':"", 'type':'string', 'control': 'textedit'},
+            {'name':'background', '':"", 'type':'string', 'control': 'textedit'},
+            {'name':'icon', '':"", 'type':'string', 'control': 'textedit'},
+
             {'name': "CONTROL", 'control':'header'},
+
+            { 'name': 'type', 'default': "push", 'type': 'string', 'control': 'menu', 'values': "push,toggle,radio" },
+            { 'name':'radioGroup', 'default':"", 'type':'string', 'control': 'textedit'},
+
+
             {'name':'command', 'default':"", 'type':'source', 'control': 'textedit'},
             {'name':'commandUp', 'default':"", 'type':'source', 'control': 'textedit'},
             {'name':'parameter', 'default':"", 'type':'source', 'control': 'textedit'},
+//            {'name':'state', 'default':"0", 'type':'int', 'control': 'textedit'},
             {'name':'value', 'default':1, 'type':'string', 'control': 'textedit'},
             {'name':'valueUp', 'default':0, 'type':'string', 'control': 'textedit'},
  
@@ -27,11 +39,14 @@ class WebUIWidgetButton extends WebUIWidgetControl
 
     static html()
     {
-        return "<button></button>";
+        return "<button type='button class=''></button>";
     }
 
-    requestData(data_set)   // TODO: can probably be removed
+
+
+    requestData(data_set)
     {
+        data_set.add(this.parameters.parameter);
         if(this.parameters.enableSource)
             data_set.add(this.parameters.enableSource);
     }
@@ -39,21 +54,87 @@ class WebUIWidgetButton extends WebUIWidgetControl
     button_down(evt)
     {
         let p = this.parentElement.parameters;
-        if(p.command)
-            this.parentElement.send_command(p.command, p.value, p.xindex, p.yindex);
 
-        else if(p.parameter)
-            this.parentElement.send_control_change(p.parameter, p.value, p.xindex, p.yindex);
+
+
+        if(p.type == "push")
+        {
+            if(p.parameter)
+                this.parentElement.send_control_change(p.parameter, p.value, p.xindex, p.yindex);
+            if(p.command)
+                this.parentElement.send_command(p.command, p.value, p.xindex, p.yindex);
+        }
+
+
+        else if(p.type=="toggle")
+        {
+            if(this.getAttribute("class") != "button-selected")
+            {
+                this.setAttribute("class","button-selected");
+                if(p.parameter)
+                    this.parentElement.send_control_change(p.parameter, p.value, p.xindex, p.yindex);
+                if(p.command)
+                    this.parentElement.send_command(p.command, p.value, p.xindex, p.yindex);
+
+            }
+            else
+            {
+                this.setAttribute("class","");
+                if(p.parameter)
+                    this.parentElement.send_control_change(p.parameter, p.valueUp, p.xindex, p.yindex);
+                if(p.commandUp)
+                    this.parentElement.send_command(p.commandUp, p.value, p.xindex, p.yindex);
+
+            }
+        }
+
+
+
+            else if(p.type=="radio")
+            {
+                let buttons = document.getElementsByTagName("webui-widget-button");
+                for(let b of buttons)
+                {
+                    if(b.parameters.radioGroup == p.radioGroup && b.firstChild.getAttribute("class")=="button-selected")
+                    {
+                        b.firstChild.setAttribute("class", "")
+                        let q = b.parameters;
+                        if(q.parameter)
+                            this.parentElement.send_control_change(q.parameter, q.valueUp, q.xindex, q.yindex);
+                    }
+                }
+                this.setAttribute("class", "button-selected")
+
+                if(p.parameter)
+                    this.parentElement.send_control_change(p.parameter, p.value, p.xindex, p.yindex);
+                if(p.command)
+                    this.parentElement.send_command(p.command, p.value, p.xindex, p.yindex);
+            }
     }
+
+
 
     button_up(evt)
     {
         let p = this.parentElement.parameters;
-        if(p.commandUp)
-             this.parentElement.send_command(p.commandUp, p.value, p.xindex, p.yindex);
 
-        else if(p.parameter)
-            this.parentElement.send_control_change(p.parameter, p.valueUp, p.xindex, p.yindex);
+        if(p.type == "push")
+        {
+            if(p.parameter)
+                this.parentElement.send_control_change(p.parameter, p.valueUp, p.xindex, p.yindex);
+            if(p.commandUp)
+                this.parentElement.send_command(p.commandUp, p.value, p.xindex, p.yindex);
+        }
+
+        else if(p.type=="toggle")
+        {
+
+        }
+
+        else if(p.type=="radio")
+        {
+
+        }
     }
 
     init()
@@ -61,20 +142,41 @@ class WebUIWidgetButton extends WebUIWidgetControl
         super.init();
         this.firstChild.addEventListener("mousedown", this.button_down, true);
         this.firstChild.addEventListener("mouseup", this.button_up, true);
-        this.firstChild.addEventListener("click", this.button_click, true);
     }
 
     update(d)
     {
-        this.firstChild.innerText = this.parameters.label;
-         try {
+        if(this.parameters.icon)
+            this.firstChild.innerHTML = "<img src='"+this.parameters.icon+"'>";
+        else
+            this.firstChild.innerText = this.parameters.label;
+
+        try
+        {
             if(this.parameters.enableSource)
                 this.firstChild.disabled = (this.getSource('enableSource')[0][0] == 0 ? true : false)
         }
         catch(err)
         {}
+        try {
+                if(this.parameters.parameter)
+                {
+                    let value = this.parameters.value;                   
+                    let v = this.getSource('parameter')[this.parameters.yindex][this.parameters.xindex]
+
+                    if(v == value)
+
+                        this.firstChild.setAttribute("class", "button-selected")
+                    else
+                        this.firstChild.setAttribute("class", "")
+                }
+
+        }
+        catch(err)
+        {}
     }
-};
+
+}
 
 
 
