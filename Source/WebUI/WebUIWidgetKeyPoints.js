f7b104a1bcfaecba4b1ee44e8d3e15e5cd1b6f83
Christian Balkenius <christian.balkenius@lucs.lu.se>
2022-03-14 00:35:58 +0100

    Update WebUIWidgetKeyPoints.js

--- Source/WebUI/WebUIWidgetKeyPoints.js
+++ Source/WebUI/WebUIWidgetKeyPoints.js
@@ -8,8 +8,10 @@ class WebUIWidgetKeyPoints extends WebUIWidgetGraph
 
             {'name': "PARAMETERS", 'control':'header'},
             {'name':'position', 'default':"", 'type':'source', 'control': 'textedit'},
-            {'name':'mark_start', 'default':"", 'type':'source', 'control': 'textedit'},
-            {'name':'mark_end', 'default':"", 'type':'source', 'control': 'textedit'},
+            {'name':'target', 'default':"", 'type':'source', 'control': 'textedit'},
+            {'name':'output', 'default':"", 'type':'source', 'control': 'textedit'},
+            {'name':'sequence', 'default':"", 'type':'source', 'control': 'textedit'},
+            {'name':'ranges', 'default':"", 'type':'source', 'control': 'textedit'},
             {'name': "STYLE", 'control':'header'},
             {'name':'color', 'default':"", 'type':'string', 'control': 'textedit'},
             {'name':'show_title', 'default':true, 'type':'bool', 'control': 'checkbox'},
@@ -26,63 +28,121 @@ class WebUIWidgetKeyPoints extends WebUIWidgetGraph
         this.onclick = function () { alert(this.data) }; // last matrix
     }
 
-    /*
-    requestData(data_set)
+
+
+// TODO: Flip coordinate system
+// TODO: Use ranges
+// TODO: Draw output at current position
+// TODO: Draw servo feedback (input) at current position
+
+    draw(sequence, f, start_time, end_time, mark_start, mark_end, target, output)
     {
-
-        data_set.add(this.parameters['module']+".KEYPOINTS");
-        data_set.add(this.parameters['module']+".TIMESTAMPS");
-
-    }
-*/
-
-    draw(count,channels)
-    {
-        /*
-        let w = this.width-this.format.marginLeft-this.format.marginRight;
-        let h = this.height-this.format.marginTop-this.format.marginBottom;
-        let min = this.parameters.min;
-        let max = this.parameters.max;
-        let scale = h/(max-min);
-        
-        this.canvas.setTransform(1, 0, 0, 1, -0.5, -0.5);
         this.canvas.clearRect(0, 0, this.width, this.height);
-        this.canvas.translate(this.format.marginLeft, this.format.marginTop);
 
-        this.canvas.lineWidth = 1;
-        this.canvas.strokeStyle = "gray";
-        this.canvas.rect(0, 0, w, h);
-        this.canvas.stroke();
-        this.canvas.clip();
+        let n = sequence.keypoints.length;
+        let channels = n != 0 ? sequence.keypoints[0].point.length : 0;
 
-        let max_time = Math.max(...this.timestamps);
+        // Draw keypoint locations
 
-        this.canvas.lineWidth = 3; //this.format.lineWidth;
-//        this.canvas.lineCap = this.format.lineCap;
-//        this.canvas.lineJoin = this.format.lineJoin;
-
-        for(let k=0; k<channels; k++)
+        for(let i=0; i<n; i++)
         {
-            this.setColor(k);
+            let pos = this.width*sequence.keypoints[i].time/end_time;
+            this.canvas.setLineDash([]);
+            this.canvas.lineWidth = 0.25;
             this.canvas.beginPath();
-            this.canvas.moveTo(0, scale*(this.keypoints[k][0]-min));
-            for(let i=1; i<count; i++)
+            this.canvas.moveTo(pos, 0);
+            this.canvas.lineTo(pos, this.height);
+            this.canvas.closePath();
+            this.canvas.stroke();
+        }
+
+        // Draw lines
+        
+        for(let c=0; c<channels;c++)
+        {
+            this.canvas.lineWidth = 0.25;
+
+            let y = this.height * (sequence.keypoints[0].point[c]/360);
+            let pos = this.width*sequence.keypoints[0].time/end_time;
+            this.canvas.setLineDash([3]);
+            this.canvas.beginPath();
+            this.canvas.moveTo(0, y);
+            this.canvas.lineTo(pos, y);
+            this.canvas.stroke();
+
+            this.canvas.setLineDash([]);
+            this.canvas.beginPath();
+            this.canvas.moveTo(pos, y);
+            for(let i=0; i<n; i++)
             {
-                let x = (w/count)*i;
-                this.canvas.lineTo(x, scale*(this.keypoints[i][k]-min));
+                let pos = this.width*sequence.keypoints[i].time/end_time;
+                let y = this.height * (sequence.keypoints[i].point[c]/360);
+                this.canvas.lineTo(pos, y);
             }
-            for(let i=1; i<count; i++)
+            this.canvas.stroke();
+
+            pos = this.width*sequence.keypoints[n-1].time/end_time;
+            y = this.height * (sequence.keypoints[n-1].point[c]/360);
+            this.canvas.setLineDash([3]);
+            this.canvas.beginPath();
+            this.canvas.moveTo(99999999, y);
+            this.canvas.lineTo(pos, y);
+            this.canvas.stroke();
+        }
+
+        // Draw points
+    
+        for(let c=0; c<channels;c++)
+        {
+            for(let i=0; i<n; i++)
             {
-           //     this.canvas.arc();
+                let pos = this.width*sequence.keypoints[i].time/end_time;
+                this.canvas.setLineDash([]);
+                this.canvas.lineWidth = 1.0;
+                this.canvas.beginPath();
+                let y = this.height * (sequence.keypoints[i].point[c]/360);
+                this.canvas.arc(pos, y, 3, 0, 2 * Math.PI, false);
+                this.canvas.fill();
             }
         }
-        */
-    }
+
+        // Draw target
 
 
-    draw(f,mark_start, mark_end)
-    {
-        this.canvas.clearRect(0, 0, this.width, this.height);
+        for(let c=0; c<channels;c++)
+        {
+            let pos = f*this.width;
+            this.canvas.setLineDash([]);
+            this.canvas.lineWidth = 1.0;
+            this.canvas.beginPath();
+            let y = this.height * (target[c]/360);
+            this.canvas.arc(pos, y, 7, 0, 2 * Math.PI, false);
+            this.canvas.strokeStyle = 'red';
+            this.canvas.stroke();
+        }
+
+
+   // Draw output
+
+
+   for(let c=0; c<channels;c++)
+   {
+       let pos = f*this.width;
+       this.canvas.setLineDash([]);
+       this.canvas.lineWidth = 1.0;
+       this.canvas.beginPath();
+       let y = this.height * (output[c]/360);
+       this.canvas.arc(pos, y, 4, 0, 2 * Math.PI, false);
+       this.canvas.fillStyle = 'red';
+       this.canvas.fill();
+   }
+
+
+
+
+        // Draw positions
+
+        this.canvas.lineWidth = 2;
         this.setColor(0);
         this.canvas.setLineDash([]);
         this.canvas.beginPath();
@@ -96,8 +156,8 @@ class WebUIWidgetKeyPoints extends WebUIWidgetGraph
             this.setColor(1);
             this.canvas.setLineDash([3]);
             this.canvas.beginPath();
-            this.canvas.moveTo(mark_start*this.width, 0);
-            this.canvas.lineTo(mark_start*this.width, this.height);
+            this.canvas.moveTo(mark_start/end_time*this.width, 0);
+            this.canvas.lineTo(mark_start/end_time*this.width, this.height);
             this.canvas.closePath();
             this.canvas.stroke();
         }
@@ -107,25 +167,29 @@ class WebUIWidgetKeyPoints extends WebUIWidgetGraph
             this.setColor(2);
             this.canvas.setLineDash([3]);
             this.canvas.beginPath();
-            this.canvas.moveTo(mark_end*this.width, 0);
-            this.canvas.lineTo(mark_end*this.width, this.height);
+            this.canvas.moveTo(mark_end/end_time*this.width, 0);
+            this.canvas.lineTo(mark_end/end_time*this.width, this.height);
             this.canvas.closePath();
             this.canvas.stroke();
         }
-
     }
 
     update(d)
     {
         if(!d)
             return;
-        
         try {
 
             let f = this.getSource("position");
-            let mark_start = this.getSource("mark_start");
-            let mark_end = this.getSource("mark_end");
-            this.draw(f[0],mark_start, mark_end);
+            let target = this.getSource("target");
+            let output = this.getSource("output");
+            let sequence = this.getSource("sequence");
+            this.data = target;
+            let start_time = sequence["start_time"];
+            let end_time = sequence["end_time"];
+            let start_mark_time = sequence["start_mark_time"];
+            let end_mark_time = sequence["end_mark_time"];
+            this.draw(sequence, f[0], start_time, end_time, start_mark_time, end_mark_time, target[0], output[0]);
         }
         catch(err)
         {
