39ce7f41eaf4aa32aeba9363008cc4c3cfe40da9
Birger Johansson <m@birgerjohansson.com>
2022-04-13 14:34:27 +0200

    Adding one hot vector module

---
+++ Source/Modules/UtilityModules/OneHotVector/OneHotVector.h
@@ -0,0 +1,50 @@
+//
+//	OneHotVector.h		This file is a part of the IKAROS project
+// 						
+//    Copyright (C) 2022 Birger Johansson
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//    See http://www.ikaros-project.org/ for more information.
+//
+
+
+#ifndef OneHotVector_
+#define OneHotVector_
+
+#include "IKAROS.h"
+
+class OneHotVector: public Module
+{
+public:
+    static Module * Create(Parameter * p) { return new OneHotVector(p); }
+
+    OneHotVector(Parameter * p) : Module(p) {}
+    virtual ~OneHotVector() {}
+
+    void 		Init();
+    void 		Tick();
+
+    float *     input_array;
+    int         input_array_size;
+
+    float *     output_array;
+    int         output_array_size;
+
+    float       value;
+};
+
+#endif
+
