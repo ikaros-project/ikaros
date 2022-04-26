0e09c2eb73498c1b291767ccf60afda9af01fc30
Birger Johansson <m@birgerjohansson.com>
2022-04-26 09:30:58 +0200

    Revert "Set current file in browser window title"
    This reverts commit 35882565db8a9bd181ee9a080cddc889ec23356c.

--- Source/WebUI/webui.js
+++ Source/WebUI/webui.js
@@ -1772,8 +1772,6 @@ controller = {
                 console.log("console.update: no state - setting pause");
                 controller.run_mode = 'pause';
             }
-            controller.ikg_file_name = "Ikaros - "+response.attributes["ikg-file-name"].replace(".ikg", "");
-            document.title = controller.ikg_file_name;
             controller.session_id = session_id;
             controller.tick = response.iteration;
             nav.init(response);
