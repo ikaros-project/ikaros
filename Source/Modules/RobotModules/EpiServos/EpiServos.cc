d3df7b158743b2a542e52faed5cf6b6f82494a62
Birger Johansson <m@birgerjohansson.com>
2022-05-17 14:26:19 +0200

    Decrease I for PID for servos

--- Source/Modules/RobotModules/EpiServos/EpiServos.cc
+++ Source/Modules/RobotModules/EpiServos/EpiServos.cc
@@ -848,7 +848,7 @@ bool EpiServos::SetDefaultSettingServo()
     }
 
     // I
-    param_default_2Byte = 500;
+    param_default_2Byte = 300;
     for (int i = HEAD_ID_MIN; i <= HEAD_ID_MAX; i++)
         if (COMM_SUCCESS != packetHandlerHead->write2ByteTxRx(portHandlerHead, i, ADDR_I, param_default_2Byte, &dxl_error))
             return false;
