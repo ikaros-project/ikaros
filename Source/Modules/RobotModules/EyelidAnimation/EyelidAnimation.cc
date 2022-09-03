22f25e401499d15c34c0e63b545aad0c1ac9ef3a
Trond <trond@EpiGrayBrain.local>
2022-09-03 16:53:50 +0200

    add shuteye func

--- Source/Modules/RobotModules/EyelidAnimation/EyelidAnimation.cc
+++ Source/Modules/RobotModules/EyelidAnimation/EyelidAnimation.cc
@@ -35,7 +35,7 @@ using namespace ikaros;
 void
 EyelidAnimation::Init()
 {
-    Bind(parameter, "parameter1");
+    Bind(parameter, "size");
 	Bind(debugmode, "debug");    
 
     input_array = GetInputArray("INPUT");
@@ -67,10 +67,42 @@ EyelidAnimation::Tick()
 	}
 }
 
-
+void shuteye(float* r, float a, int sz){
+  // a: 0-1 degree of shut
+  // diodes indexed clockwise from 0 degrees; 0-sz/2 bottom; sz/2-sz top
+  color[] retval = new color[sz];
+  
+  // translate degree to colors for shuteye
+  color dark = #666666;
+  color light = #bbbbbb;
+  for(int i=0; i < sz; i++) retval[i] = light;
+  // 0 - all light
+  if(a >= 0 && a < 0.2) return retval;
+  else if(a >= 0.2 && a < 0.4){
+    // 0.25 - 2,3 and 8,9 dark
+    int[] ix = {1,2,7,8};
+    for (int i=0; i<ix.length; i++) retval[ix[i]] = dark;
+  }
+  else if(a >= 0.4 && a < 0.6){
+    // 0.5 - 1234 and 789a
+    int[] ix = {0,1,2,3,6,7,8,9};
+    for (int i=0; i<ix.length; i++) retval[ix[i]] = dark;
+  }
+  else if(a >= 0.6 && a < 0.8){
+    // 0.75 - 012345 and 789a
+    int[] ix = {11,0,1,2,3,4, 6,7,8,9};
+    for (int i=0; i<ix.length; i++) retval[ix[i]] = dark;
+  }
+  else if (a >= 0.8){
+    // 1 - all dark
+    for (int i=0; i<sz; i++) retval[i] = dark;
+  }
+  return retval;
+  
+}
 
 // Install the module. This code is executed during start-up.
 
-static InitClass init("EyelidAnimation", &EyelidAnimation::Create, "Source/UserModules/EyelidAnimation/");
+static InitClass init("EyelidAnimation", &EyelidAnimation::Create, "Source/Modules/RobotModules/EyelidAnimation/");
 
 
