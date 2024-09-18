7f54c7532e17df329de4cc925cd03fd75b46bef7
Christian Balkenius <christian@ikaros-project.org>
2024-09-18 13:17:08 +0200

    Update ikaros.h

--- Source/ikaros.h
+++ Source/ikaros.h
@@ -231,6 +231,7 @@ public:
     void Bind(parameter & p, std::string n);   // Bind to parameter in global parameter table
     void Bind(matrix & m, std::string n); // Bind to input or output in global parameter table, or matrix parameter
 
+    virtual void SetParameters() {} // Can be overridden in modules to set parmeter values in code rather than from the ikc/ikg file; called before Init()
     virtual void Tick() {}
     virtual void Init() {}
 
