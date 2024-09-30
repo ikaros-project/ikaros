f656d2563f2f1fc41866d21b3152cf6891c5f373
Christian Balkenius <christian@ikaros-project.org>
2024-09-30 14:36:22 +0200

    H_matrices and angle conversion. Fixes #198.

--- Source/Kernel/maths.h
+++ Source/Kernel/maths.h
@@ -7,15 +7,19 @@
 
 namespace ikaros
 {
+	const double pi = 3.14159265358979323846;  // Awaiting C++20
 
 	double sgn(double x);
 
 	double min(double x, double y);
 	double max(double x, double y);
 	double clip(double x, double low, double high);
+
+	double angle_to_angle(double angle, int from_angle_unit, int to_angle_unit);
+   	double short_angle(double a1, double a2); // in radians
 };
 
-	const double pi = 3.14159265358979323846;  // Awaiting C++20
+
 
 #endif
 
