1cf4346811ac782b89a179c204875fc2bc8f781d
Christian Balkenius <christian@ikaros-project.org>
2025-02-10 23:30:54 +0100

    KernelTest modules

--- Source/Modules/UtilityModules/KernelTestModules/KernelSizeTestModule/KernelSizeTestModule.cc
+++ Source/Modules/UtilityModules/KernelTestModules/KernelSizeTestModule/KernelSizeTestModule.cc
@@ -4,17 +4,32 @@ using namespace ikaros;
 
 class KernelSizeTestModule: public Module
 {
+    matrix input;
     matrix output;
 
     void Init()
     {
-        Bind(output, "OUTPUT");    
+        Bind(input, "INPUT");
+        Bind(output, "OUTPUT");
+
+        Trace("Init-Trace");
+        Print("Init-Print");
+        Warning("Init-Warning");
     }
 
 
     void Tick()
     {
-        output.print("OUTPUT"); 
+        output.print("OUTPUT");
+
+        Trace("Tick-Trace");
+        Print("Tick-Print");
+        Warning("Tick-Warning");
+
+        double x = input;
+        double y = input.last();
+
+        std::cout << "DIFF: " << x << " " << y << std::endl;
     }
 };
 
