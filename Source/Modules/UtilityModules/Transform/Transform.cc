117ec05df2e240322669f80744af38cde3e89024
Birger Johansson <m@birgerjohansson.com>
2025-02-20 21:32:21 +0100

    Transform ikaros 3 ready

--- Source/Modules/UtilityModules/Transform/Transform.cc
+++ Source/Modules/UtilityModules/Transform/Transform.cc
@@ -20,7 +20,7 @@
 //    See http://www.ikaros-project.org/ for more information.
 //
 
-// Inte klar än. Hur gör man med set size?
+// This module expeCt a Zx4x4 matrix as input.
 
 #include "ikaros.h"
 
@@ -43,8 +43,6 @@ class Transform : public Module
     matrix object_id;
     matrix frame_id;
 
-    parameter input_length;
-
     void Init()
     {
         Bind(invert_1, "invert_1");
@@ -62,13 +60,14 @@ class Transform : public Module
         Bind(object_id, "OBJECT_ID");
         Bind(frame_id, "FRAME_ID");
 
-        // Bind(size_y, "size_y");
+        if (matrix_1.rank() != 3 && matrix_1.size_x() != 4 && matrix_1.size_y() != 4)
+            Notify(msg_fatal_error, "MATRIX_1 must be a Zx4x4 matrix");
+        if (matrix_2.rank() != 3 && matrix_2.size_x() != 4 && matrix_2.size_y() != 4)
+            Notify(msg_fatal_error, "MATRIX_2 must be a Zx4x4 matrix");
     }
-
     void Tick()
     {
-       
-        // Reset input
+        // Reset output
         matrix_out.reset();
         object_id.reset();
         frame_id.reset();
@@ -81,10 +80,6 @@ class Transform : public Module
             matrix_out(i, 3, 3) = 1;
         }
 
-        // reset_matrix(matrix_out, 16, size_y);
-        // reset_array(object_id, size_y);
-        // reset_array(frame_id, size_y);
-
         h_matrix a, b;
         h_matrix m1, m2; // Using h_matrix
         m1.set_name("M1");
@@ -94,58 +89,16 @@ class Transform : public Module
         int k = 0; // target location in output
         float o1, f1, o2, f2;
 
-        //if (path_ == "ExperimentalSetup.Epi.ForwardModel.PosBody")
-        // if (path_ == "ExperimentalSetup.Epi.ForwardModel.BodyHead")
-        // {
-        //     std::cout << path_ << std::endl;
-
-        //     std::cout << "matrix in size: " << matrix_1.size() << std::endl;
-        //     std::cout << "matrix in size: " << matrix_1.size_x() << " " << matrix_1.size_y() << " " << matrix_1.size_z() << std::endl;
-        //     // matrix_1.info();
-
-        //     std::cout << "matrix in size: " << matrix_2.size() << std::endl;
-        //     std::cout << "matrix in size: " << matrix_2.size_x() << " " << matrix_2.size_y() << " " << matrix_2.size_z() << std::endl;
-        //     // matrix_2.info();
-
-        //     matrix_1.print();
-        //     matrix_2.print();
-        //     // matrix_out.print();
-        //     std::cout << "matrix out size: " << matrix_out.size_x() << " " << matrix_out.size_y() << " " << matrix_out.size_z() << std::endl;
-
-
-        //     // invert_1.print();
-        //     // invert_2.print();
-        //     // object_id_1.print();
-        //     // frame_id_1.print();
-        //     // object_id_2.print();
-        //     // frame_id_2.print();
-        //     // object_id.print();
-        //     // frame_id.print();
-
-        //     object_id_1.print();
-        //     frame_id_1.print();
-        //     object_id_2.print();
-        //     frame_id_2.print();
-        // }
-
         // Loop over length of input matrix.
-        for (int i = 0; i < matrix_1.size_z(); i++) // Detta kan ju inte funka?
+        for (int i = 0; i < matrix_1.size_z(); i++) // this only works if we expect Xx4x4 matricies
         {
             m1.copy(matrix_1[i]);
-            // if (path_ == "ExperimentalSetup.Epi.ForwardModel.PosBody")
-            //     m1.print();
-
             if (m1.is_valid())
             {
                 for (int j = 0; j < matrix_2.size_z(); j++)
                 {
-
                     m2.copy(matrix_2[j]);
 
-                    // if (path_ == "ExperimentalSetup.Epi.ForwardModel.PosBody")
-                    // {
-                    //     m2.print();
-                    // }
                     o1 = (invert_1 ? frame_id_1[i] : object_id_1[i]);
                     f1 = (invert_1 ? object_id_1[i] : frame_id_1[i]);
 
@@ -156,23 +109,16 @@ class Transform : public Module
                     {
                         if (invert_1)
                             a.inv(m1);
-                        // h_inv(a, matrix_1[i]);
                         else
                             a.copy(m1);
-                        // a.copy(matrix_2[j]); // Test Funkar. Det betyder att argumentet inte kan vara h_matrix. Ska man typcasta denna då?
 
                         if (invert_2)
                             b.inv(m2);
-
-                        // h_inv(b, matrix_2[j]);
                         else
                             b.copy(m2);
 
-                        // h_copy(b, matrix_2[j]);
-
                         matrix_out[k].matmul(a, b);
 
-                        // h_multiply(matrix_out[k], a, b);
                         object_id[k] = o2;
                         frame_id[k] = f1;
 
@@ -181,16 +127,6 @@ class Transform : public Module
                 }
             }
         }
-        // if (path_ == "ExperimentalSetup.Epi.ForwardModel.BodyHead")
-        // {
-        //     std::cout << "************* END ***************** " << std::endl;
-        //     matrix_1.print();
-        //     matrix_2.print();
-        //     matrix_out.print();
-
-        //     object_id.print();
-        //     frame_id.print();
-        // }
     }
 };
 
