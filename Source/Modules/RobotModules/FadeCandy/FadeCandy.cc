a2d518df0827d681ce60c967b00dd568c24ea92c
Birger Johansson <m@birgerjohansson.com>
2025-02-27 11:07:52 +0100

    Update fadecandy
    Better example file and code that can reconnect to USB device.

--- Source/Modules/RobotModules/FadeCandy/FadeCandy.cc
+++ Source/Modules/RobotModules/FadeCandy/FadeCandy.cc
@@ -34,7 +34,13 @@ class FadeCandy : public Module
     matrix RightEye, LeftEye, MouthLow, MouthHigh;
     fadecandy_driver::FadecandyDriver fd_driver;
 
-    size_t c = 0;
+    // Initialize the led array colors
+    std::vector<std::vector<Color>> led_array_colors = {
+        std::vector<Color>(8, Color(0, 0, 0)),  // (Mouth high)
+        std::vector<Color>(8, Color(0, 0, 0)),  // (Mouth low)
+        std::vector<Color>(12, Color(0, 0, 0)), // (Right eye)
+        std::vector<Color>(12, Color(0, 0, 0))  // (Left eye)
+    };
 
     void Init()
     {
@@ -44,62 +50,71 @@ class FadeCandy : public Module
         Bind(MouthHigh, "MOUTH_HIGH");
         Bind(MouthLow, "MOUTH_LOW");
 
-        if ((LeftEye.size_x() != 12 && LeftEye.size_y() != 3))
-            Notify(msg_fatal_error, "LEFT_EYE size is not 3x12");
-        if ((RightEye.size() != 12 && RightEye.size_y() != 3))
-            Notify(msg_fatal_error, "RIGHT_EYE size is not 3x12");
-        if ((MouthHigh.size() != 8 && MouthHigh.size_y() != 3))
-            Notify(msg_fatal_error, "MOUTH_HIGH size is not 3x8");
-        if ((MouthLow.size() != 8 && MouthLow.size_y() != 3))
-            Notify(msg_fatal_error, "MOUTH_LOW size is not 3x8");
+        if ((LeftEye.size_x() <= 12 && LeftEye.size_y() != 3))
+            Notify(msg_warning, "Input LEFT_EYE size is not 3x12");
+        if ((RightEye.size() <= 12 && RightEye.size_y() != 3))
+            Notify(msg_warning, "Input RIGHT_EYE size is not 3x12");
+        if ((MouthHigh.size() <= 8 && MouthHigh.size_y() != 3))
+            Notify(msg_warning, "Input MOUTH_HIGH size is not 3x8");
+        if ((MouthLow.size() <= 8 && MouthLow.size_y() != 3))
+            Notify(msg_warning, "Input MOUTH_LOW size is not 3x8");
 
         try
         {
             auto serial_number = fd_driver.connect();
-            Notify(msg_debug, "Connected to Fadecandy board");
+            Notify(msg_debug, "Connected to Fadecandy board: " + serial_number);
         }
         catch (const std::exception &e)
         {
-            Notify(msg_warning, "Could not connect to Fadecandy board");
-        }
-
-        if (!fd_driver.isConnected())
-        {
-            Notify(msg_warning, "Not connected to Fadecandy board");
+            Notify(msg_warning, "Could not connect to Fadecandy board"); // Should this be fatal?
             return;
         }
     }
 
-    void Tick()
+    void
+    Tick()
     {
-        std::vector<std::vector<Color>> led_array_colors = {
-            std::vector<Color>(8, Color(0, 0, 0)),  // (Left eye)
-            std::vector<Color>(8, Color(0, 0, 0)),  // (Right eye)
-            std::vector<Color>(12, Color(0, 0, 0)), // (Mouth high)
-            std::vector<Color>(12, Color(0, 0, 0))  // (Mouth low)
-        };
+        if (!fd_driver.isConnected())
+        {
+            try
+            {
+                Notify(msg_debug, "Reconnecting to Fadecandy board: ");
+                auto serial_number = fd_driver.connect();
+                Notify(msg_warning, "Reconnecting to Fadecandy board: " + serial_number);
+            }
+            catch (const std::exception &e)
+            {
+                Notify(msg_debug, "Could not connect to Fadecandy board");
+                return;
+            }
+        }
 
         // Fill color from input
-        for (size_t i = 0; i < 8; ++i)
+        for (size_t i = 0; i < 8; ++i) // 8 Leds in each row of the mouth
         {
-            led_array_colors[0][i] = Color(LeftEye[0][i], LeftEye[1][i], LeftEye[2][i]);
-            led_array_colors[1][i] = Color(RightEye[0][i], RightEye[1][i], RightEye[2][i]);
+            led_array_colors[1][i] = Color(MouthHigh[0][i] * 255, MouthHigh[1][i] * 255, MouthHigh[2][i] * 255);
+            led_array_colors[0][i] = Color(MouthLow[0][i] * 255, MouthLow[1][i] * 255, MouthLow[2][i] * 255);
         }
-        for (size_t i = 0; i < 8; ++i)
+        for (size_t i = 0; i < 12; ++i) // 12 Leds in each eye
         {
-            led_array_colors[2][i] = Color(MouthHigh[0][i], MouthHigh[1][i], MouthHigh[2][i]);
-            led_array_colors[3][i] = Color(MouthLow[0][i], MouthLow[1][i], MouthLow[2][i]);
+            led_array_colors[2][i] = Color(RightEye[0][i] * 255, RightEye[1][i] * 255, RightEye[2][i] * 255);
+            led_array_colors[3][i] = Color(LeftEye[0][i] * 255, LeftEye[1][i] * 255, LeftEye[2][i] * 255);
         }
 
         try
         {
-            fd_driver.setColors(led_array_colors);
+            fd_driver.setColors(led_array_colors); // Send the colors to the driver. Set color is checking that isConnected is true. However,if the device is unpluged this is not detected by the driver.
         }
         catch (const std::exception &e)
         {
-            Notify(msg_warning, "Could not set colors of the eyes");
+            Notify(msg_debug, "Could not set colors of the eyes");
         }
     }
-};
 
+    // Figer out how to handle the destructor 
+    ~FadeCandy()
+    {
+       // fd_driver.~FadecandyDriver();
+    }
+};
 INSTALL_CLASS(FadeCandy)
