81e135fdd5b202655e8eeecfc2e96364a9d051aa
Birger Johansson <m@birgerjohansson.com>
2025-03-17 09:28:43 +0100

    Adding SCALE input to scale module.
    Also better example file.

--- Source/Modules/UtilityModules/Scale/Scale.cc
+++ Source/Modules/UtilityModules/Scale/Scale.cc
@@ -3,27 +3,28 @@
 
 using namespace ikaros;
 
-class Scale: public Module
+class Scale : public Module
 {
-    matrix      input;
-    matrix      output;
-    parameter   factor;
+    matrix input;
+    matrix scale;
+    matrix output;
+    parameter factor;
 
     void Init()
     {
         Bind(input, "INPUT");
         Bind(output, "OUTPUT");
         Bind(factor, "factor");
+        Bind(scale, "SCALE");
     }
-
 
     void Tick()
     {
         output.copy(input);
         output.scale(factor);
+        if (scale.connected())
+            output.scale(scale(0));
     }
 };
 
-
 INSTALL_CLASS(Scale)
-
