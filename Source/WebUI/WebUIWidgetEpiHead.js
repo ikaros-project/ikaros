bb018d8f2c7ca9e96fcaee15bd5dfefd43ea6837
Birger Johansson <m@birgerjohansson.com>
2025-03-18 07:18:31 +0100

    Updating EpuHead

--- Source/WebUI/WebUIWidgetEpiHead.js
+++ Source/WebUI/WebUIWidgetEpiHead.js
@@ -117,8 +117,7 @@ class WebUIWidgetEpiHead extends WebUIWidgetGraph {
     }
 
 
-    draw(gaze, pupil, offset, iLRGB, iLI, iRRGB, iRI, mRGB, mI)
-     {
+    draw(gaze, pupil, offset, iLRGB, iLI, iRRGB, iRI, mRGB, mI) {
         let w = this.width;
         let h = this.height;
         let s = Math.min(this.width, this.height) / 180;
@@ -137,40 +136,16 @@ class WebUIWidgetEpiHead extends WebUIWidgetGraph {
 
         if (drawFace) {
             // right ear
-            this.setColor(0);
-            this.canvas.fillStyle = this.parameters.earColor;
+            this.canvas.fillStyle = "red";
             this.canvas.beginPath();
-            this.canvas.rect(5, 60, 10, 65);
-            this.canvas.fill();
-            this.canvas.stroke();
-
-            // right ear lid
-            this.setColor(0);
-            this.canvas.beginPath();
-            this.canvas.moveTo(155, 62.5);
-            this.canvas.lineTo(160, 67.5);
-            this.canvas.lineTo(160, 117.5);
-            this.canvas.lineTo(155, 122.5);
-            this.canvas.closePath();
+            this.canvas.rect(3, 60, 12, 65);
             this.canvas.fill();
             this.canvas.stroke();
 
             // left ear
-            this.setColor(0);
-            this.canvas.fillStyle = this.parameters.earColor;
+            this.canvas.fillStyle = "blue";
             this.canvas.beginPath();
-            this.canvas.rect(145, 60, 10, 65);
-            this.canvas.fill();
-            this.canvas.stroke();
-
-            // left ear lid
-            this.setColor(0);
-            this.canvas.beginPath();
-            this.canvas.moveTo(5, 62.5);
-            this.canvas.lineTo(0, 67.5);
-            this.canvas.lineTo(0, 117.5);
-            this.canvas.lineTo(5, 122.5);
-            this.canvas.closePath();
+            this.canvas.rect(145, 60, 12, 65);
             this.canvas.fill();
             this.canvas.stroke();
 
@@ -187,28 +162,10 @@ class WebUIWidgetEpiHead extends WebUIWidgetGraph {
             this.canvas.lineTo(15, 40);
             this.canvas.quadraticCurveTo(15, 0, 50, 0);
             this.canvas.closePath();
-
-            // Left Ear Lid
-            this.canvas.moveTo(5, 62.5);
-            this.canvas.lineTo(0, 67.5);
-            this.canvas.lineTo(0, 117.5);
-            this.canvas.lineTo(5, 122.5);
-            this.canvas.closePath();
-
-            // Right Ear Lid
-            this.canvas.moveTo(155, 62.5);
-            this.canvas.lineTo(160, 67.5);
-            this.canvas.lineTo(160, 117.5);
-            this.canvas.lineTo(155, 122.5);
-            this.canvas.closePath();
-
             this.canvas.fill();
             this.canvas.stroke();
 
             // Mouth
-
-
-
             if (mRGB)
                 this.canvas.fillStyle = 'rgba('
                     + Math.floor(255 * mRGB[0]) + ','
@@ -218,6 +175,7 @@ class WebUIWidgetEpiHead extends WebUIWidgetGraph {
             else
                 this.canvas.fillStyle = this.parameters.mouthColor;
 
+            // Stroke
             for (let c = 0; c < 2; c++)
                 for (let i = 0; i < 6; i++) {
                     this.canvas.beginPath();
@@ -227,10 +185,184 @@ class WebUIWidgetEpiHead extends WebUIWidgetGraph {
                 }
         }
 
-        // eyes
-        this.drawEye(22.5, 72.5, gaze[1], pupil[1], iLRGB, iLI);
-        this.drawEye(85, 72.5, gaze[0], pupil[0], iRRGB, iRI);
+        // Eyes
+        // ====
 
+        // Left eyes
+        // Eye outline
+        this.canvas.save();
+        this.canvas.translate(22.5, 72.5);
+        this.setColor(0);
+        this.canvas.fillStyle = "white";
+        this.canvas.beginPath();
+        this.canvas.moveTo(37.5, 0);
+        this.canvas.quadraticCurveTo(52.5, 0, 52.5, 15);
+        this.canvas.lineTo(52.5, 25);
+        this.canvas.quadraticCurveTo(52.5, 40, 37.5, 40);
+        this.canvas.lineTo(16, 40);
+        this.canvas.quadraticCurveTo(0, 40, 0, 25);
+        this.canvas.lineTo(0, 15);
+        this.canvas.quadraticCurveTo(0, 0, 15, 0);
+        this.canvas.closePath();
+        this.canvas.fill();
+        this.canvas.clip();
+
+        // Should be matched with Epi!
+        let v = 27.5 * Math.sin(gaze[1] * Math.PI / 180)
+
+        // Pupil
+        this.canvas.fillStyle = "black";
+        this.canvas.beginPath();
+        this.canvas.arc(27.5 + v, 20, pupil[1] / 18 * (17.5 * 0.5625), 0, 2 * Math.PI);
+        this.canvas.fill();
+        this.canvas.restore();
+
+        // Draw the left NeoPixel ring
+{
+        const leftNeoPixelRing = [];
+        const leftCenterX = 22.5 + 27.5;
+        const leftCenterY = 72.5 + 20;
+        const leftRadius = 8 + 5; // Radius of the iris plus some offset for the NeoPixel ring
+        const leftLedRadius = 3.5;
+        const leftNumLEDs = 12;
+
+        for (let i = 0; i < leftNumLEDs; i++) {
+            const leftAngle = (i / leftNumLEDs) * 2 * Math.PI;
+            const leftX = leftCenterX + leftRadius * Math.cos(leftAngle);
+            const leftY = leftCenterY + leftRadius * Math.sin(leftAngle);
+            leftNeoPixelRing.push({ x: leftX, y: leftY });
+        }
+
+        leftNeoPixelRing.forEach(leftLed => {
+            this.canvas.beginPath();
+            this.canvas.arc(leftLed.x, leftLed.y, leftLedRadius, 0, 2 * Math.PI);
+            this.canvas.fillStyle = '#00AFFF';
+            this.canvas.fill();
+        });
+
+        // Add Led diffuser
+        this.canvas.fillStyle = "rgba(255, 255, 255, 0.8)";
+        this.canvas.beginPath();
+        this.canvas.arc(leftCenterX, leftCenterY, 18, 0, 2 * Math.PI);
+        this.canvas.fill();
+
+        this.canvas.restore();
+
+        // Draw outline again
+        this.canvas.save();
+        this.canvas.translate(22.5, 72.5);
+        this.setColor(0);
+        this.canvas.beginPath();
+        this.canvas.moveTo(37.5, 0);
+        this.canvas.quadraticCurveTo(52.5, 0, 52.5, 15);
+        this.canvas.lineTo(52.5, 25);
+        this.canvas.quadraticCurveTo(52.5, 40, 37.5, 40);
+        this.canvas.lineTo(16, 40);
+        this.canvas.quadraticCurveTo(0, 40, 0, 25);
+        this.canvas.lineTo(0, 15);
+        this.canvas.quadraticCurveTo(0, 0, 15, 0);
+        this.canvas.closePath();
+        this.canvas.stroke();
+        this.canvas.restore();
+    }
+
+
+
+        // Right eyes
+        // Eye outline
+        this.canvas.save();
+        this.canvas.translate(85, 72.5);
+        this.setColor(0);
+        this.canvas.fillStyle = "white";
+        this.canvas.beginPath();
+        this.canvas.moveTo(37.5, 0);
+        this.canvas.quadraticCurveTo(52.5, 0, 52.5, 15);
+        this.canvas.lineTo(52.5, 25);
+        this.canvas.quadraticCurveTo(52.5, 40, 37.5, 40);
+        this.canvas.lineTo(16, 40);
+        this.canvas.quadraticCurveTo(0, 40, 0, 25);
+        this.canvas.lineTo(0, 15);
+        this.canvas.quadraticCurveTo(0, 0, 15, 0);
+        this.canvas.closePath();
+        this.canvas.fill();
+        this.canvas.clip();
+
+        // Should be matched with Epi!
+        v = 27.5 * Math.sin(gaze[1] * Math.PI / 180)
+
+        // Iris
+        // if (iLRGB)
+        //     this.canvas.fillStyle = 'rgb('
+        //         + Math.floor(255 * iLRGB[0]) + ','
+        //         + Math.floor(255 * iLRGB[1]) + ','
+        //         + Math.floor(255 * iLRGB[2]) + ','
+        //         + iRI + ')'
+
+        // else
+        //     this.canvas.fillStyle = this.parameters.irisColor; // when LED input not connected
+
+        // this.canvas.strokeStyle = "#00000000";
+        // this.canvas.beginPath();
+        // this.canvas.arc(27.5 + v, 20, 17.5, 0, 2 * Math.PI);
+        // this.canvas.fill();
+
+        // Pupil
+
+        this.canvas.fillStyle = "black";
+        this.canvas.beginPath();
+        this.canvas.arc(27.5 + v, 20, pupil[1] / 18 * (17.5 * 0.5625), 0, 2 * Math.PI);
+        this.canvas.fill();
+        this.canvas.restore();
+
+        // Draw the left NeoPixel ring
+
+        const rightNeoPixelRing = [];
+        const rightCenterX = 85 + 27.5;
+        const rightCenterY = 72.5 + 20;
+        const rightRadius = 8 + 5; // Radius of the iris plus some offset for the NeoPixel ring
+        const rightLedRadius = 3.5;
+        const rightNumLEDs = 12;
+
+        for (let i = 0; i < rightNumLEDs; i++) {
+            const rightAngle = (i / rightNumLEDs) * 2 * Math.PI;
+            const rightX = rightCenterX + rightRadius * Math.cos(rightAngle);
+            const rightY = rightCenterY + rightRadius * Math.sin(rightAngle);
+            rightNeoPixelRing.push({ x: rightX, y: rightY });
+        }
+
+        rightNeoPixelRing.forEach(rightLed => {
+            this.canvas.beginPath();
+            this.canvas.arc(rightLed.x, rightLed.y, rightLedRadius, 0, 2 * Math.PI);
+            this.canvas.fillStyle = '#00AFFF';
+            this.canvas.fill();
+        });
+
+        // Add Led diffuser
+        this.canvas.fillStyle = "rgba(255, 255, 255, 0.8)";
+        this.canvas.beginPath();
+        this.canvas.arc(rightCenterX, rightCenterY, 18, 0, 2 * Math.PI);
+        this.canvas.fill();
+
+
+        this.canvas.restore();
+
+
+        // Draw outline again
+        this.canvas.save();
+        this.canvas.translate(85, 72.5);
+        this.setColor(0);
+        this.canvas.beginPath();
+        this.canvas.moveTo(37.5, 0);
+        this.canvas.quadraticCurveTo(52.5, 0, 52.5, 15);
+        this.canvas.lineTo(52.5, 25);
+        this.canvas.quadraticCurveTo(52.5, 40, 37.5, 40);
+        this.canvas.lineTo(16, 40);
+        this.canvas.quadraticCurveTo(0, 40, 0, 25);
+        this.canvas.lineTo(0, 15);
+        this.canvas.quadraticCurveTo(0, 0, 15, 0);
+        this.canvas.closePath();
+        this.canvas.stroke();
+        this.canvas.restore();
     }
 
 
@@ -275,19 +407,19 @@ class WebUIWidgetEpiHead extends WebUIWidgetGraph {
             let iRRGB = this.getSourceAsArray('irisRightRGB');
             let mRGB = this.getSourceAsArray('mouthRGB');
 
-            let iLI = this.getSourceAsFloat('irisLeftIntensity',[[1]]);
-            let iRI = this.getSourceAsFloat('irisRightIntensity',[[1]]);
-            let mI = this.getSourceAsFloat('mouthIntensity',[[1]]);
-                       
+            let iLI = this.getSourceAsFloat('irisLeftIntensity', [[1]]);
+            let iRI = this.getSourceAsFloat('irisRightIntensity', [[1]]);
+            let mI = this.getSourceAsFloat('mouthIntensity', [[1]]);
+
             if (gaze.length < 2)
                 gaze = [gaze[0], gaze[0]]
 
             if (headPosition.length < 2)
                 headPosition = [headPosition[0], 0]
-            
-            this.draw(gaze, pupil, headPosition, iLRGB, iLI, iRRGB, iRI, mRGB, mI);
 
-            }
+            this.draw(gaze, pupil, headPosition, iLRGB, iLI, iRRGB, iRI, mRGB, mI);
+            //this.draw();
+        }
 
     }
 };
