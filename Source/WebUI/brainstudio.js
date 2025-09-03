c2796f8e0e21efdb302cdb0d0b319c5e231535a2
ikaros-project <christian@ikaros-project.org>
2025-09-03 21:06:16 +0200

    Update brainstudio.js

--- Source/WebUI/brainstudio.js
+++ Source/WebUI/brainstudio.js
@@ -170,7 +170,7 @@ function formatTime(d)
         if(d < 0)
             return "-";
         else if(d < 0.001)
-            return Math,round(1000000*d) + " &#181;s";
+            return Math.round(1000000*d) + " &#181;s";
         else if(d < 5)
             return Math.round(1000*d) + " ms";
         else
