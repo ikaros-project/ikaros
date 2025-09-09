6d69aaa64c77f13b45a89c2d2de640794752fbaa
ikaros-project <christian@ikaros-project.org>
2025-09-09 12:55:24 +0200

    Arbiter - not working yet

--- Source/Modules/UtilityModules/Arbiter copy/Arbiter.h
+++ Source/Modules/UtilityModules/Arbiter copy/Arbiter.h
@@ -1,4 +1,22 @@
-//		Arbiter.h		This file is a part of the IKAROS project Copyright (C) 2006-2018 Christian Balkenius
+//
+//		Arbiter.h		This file is a part of the IKAROS project
+//
+//    Copyright (C) 2006-2018 Christian Balkenius
+///
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
 
 // input: 2 x (vector weight)
 // output: (vector with max weight (output) and the max veight (value))
