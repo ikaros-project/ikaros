b9e8db3a826fe4259695db73a8f79cfc1135ce90
ikaros-project <christian@ikaros-project.org>
2026-04-18 23:39:40 +0200

    Update FFMpegGrab.cc

--- Source/Modules/IOModules/Video/InputVideoStream/FFMpegGrab.cc
+++ Source/Modules/IOModules/Video/InputVideoStream/FFMpegGrab.cc
@@ -1,5 +1,5 @@
 //
-//	  FFMpegGrab.cc		This file is a part of the IKAROS project
+//    FFMpegGrab.cc        This file is a part of the IKAROS project
 //
 //    Copyright (C) 2018 Birger Johansson
 //
@@ -19,279 +19,427 @@
 //
 //    See http://www.ikaros-project.org/ for more information.
 //
-//
-// Code inspired from:
-// https://www.ffmpeg.org/doxygen/2.8/examples.html
-// http://dranger.com/ffmpeg/
-// https://sourceforge.net/u/leixiaohua1020/profile/
-// https://blogs.gentoo.org/lu_zero/2016/03/29/new-avcodec-api/
 
 #include "FFMpegGrab.h"
 #include "ikaros.h"
 
-using namespace ikaros;
+#include <condition_variable>
+#include <cstring>
+#include <iostream>
+#include <mutex>
+#include <thread>
+#include <utility>
+#include <vector>
 
-FFMpegGrab::FFMpegGrab()
+extern "C"
 {
-	if (printInfo)
-		std::cout << "FFMpegGrab()" << std::endl;
-
-	avformat_network_init();
-	av_log_set_level(AV_LOG_INFO); // This must be set to be able to use av_dump_format. Later this wil be set to AV_LOG_ERROR
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+#include <libavutil/imgutils.h>
 }
 
-bool FFMpegGrab::Init()
+using namespace ikaros;
+
+namespace
 {
-	if (printInfo)
-		std::cout << "FFMpegGrab Init()" << std::endl;
+int
+decode_packet(AVCodecContext * avctx, AVFrame * frame, int * got_frame, AVPacket * pkt)
+{
+    int ret;
+    *got_frame = 0;
+    if(pkt)
+    {
+        ret = avcodec_send_packet(avctx, pkt);
+        if(ret != 0)
+            return ret == AVERROR_EOF ? 0 : ret;
+    }
 
-	const AVInputFormat *file_iformat = NULL;
-	// uv4l uses raw h264
-	if (uv4l)
-		file_iformat = av_find_input_format("h264");
+    ret = avcodec_receive_frame(avctx, frame);
+    if(ret != 0 && ret != AVERROR(EAGAIN) && ret != AVERROR_EOF)
+        return ret;
 
-	AVDictionary *options = NULL;
+    if(ret == 0)
+        *got_frame = 1;
 
-	if (uv4l)
-		av_dict_set(&options, "probesize", "192", 0);
+    return 0;
+}
 
-	/// Open video file
-	if (avformat_open_input(&input_format_context, url, file_iformat, &options) != 0) // Allocating input_format_context
-	{
-		std::cout << "FFMpegGrab: Could not open file " << url << std::endl;
-		return false;
-	}
-	av_dict_free(&options);
+}
 
-	// Retrieve stream information
-	// if(avformat_find_stream_info(input_format_context, NULL) < 0)
-	if (av_find_best_stream(input_format_context, AVMEDIA_TYPE_VIDEO, 0, 0, NULL, 0))
-	{
-		std::cout << "FFMpegGrab: Couldn't find stream information" << std::endl;
-		return false;
-	}
+struct FFMpegGrab::Impl
+{
+    AVFormatContext * input_format_context = nullptr;
+    int videoStreamId = -1;
+    const AVCodec * input_codec = nullptr;
+    AVCodecContext * avctx = nullptr;
+    AVFrame * inputFrame = nullptr;
+    AVFrame * outputFrame = nullptr;
+    AVPacket packet{};
+    int outputSizeX = 0;
+    int outputSizeY = 0;
+    SwsContext * img_convert_ctx = nullptr;
 
-	if (printInfo)
-		av_dump_format(input_format_context, 0, url, 0);
+    std::vector<uint8_t> frame;
 
-	av_log_set_level(AV_LOG_ERROR);
+    bool uv4l = false;
+    std::string url;
+    bool printInfo = false;
+    bool synchronized = false;
+    bool freshData = false;
+    bool shutdown = false;
+    float FPS = 0.0f;
+    Timer timer;
 
-	/// Find the first video stream
-	videoStreamId = -1;
-	for (int i = 0; i < input_format_context->nb_streams; i++)
-	{
-		if (input_format_context->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_VIDEO)
-		{
-			videoStreamId = i;
-			break;
-		}
-	}
-	if (videoStreamId == -1)
-	{
-		std::cout << "FFMpegGrab: Didn't find a video stream" << std::endl;
-		return false;
-	}
+    std::mutex state_mutex;
+    std::condition_variable state_cv;
+    std::thread worker;
+};
 
-	// Prepare decoder
-	if (uv4l)
-	{
-		input_codec = avcodec_find_decoder_by_name("h264_mmal"); // Using mmal for uv4l for raspberry pi
-		if (input_codec)
-		{
-			std::cout << "FFMpegGrab: Using HW decoding (mmal)" << std::endl;
-		}
-		else
-			input_codec = avcodec_find_decoder(AV_CODEC_ID_H264);
-	}
-	else // Other stream formats
-		input_codec = avcodec_find_decoder(input_format_context->streams[videoStreamId]->codecpar->codec_id);
-
-	if (input_codec == NULL)
-	{
-		std::cout << "FFMpegGrab: Unsupported codec!" << std::endl;
-		return false;
-	}
-
-	// Creating decoding context
-	avctx = avcodec_alloc_context3(input_codec);
-	if (!avctx)
-	{
-		std::cout << "FFMpegGrab: Could not allocate a decoding context" << std::endl;
-		avformat_close_input(&input_format_context);
-		return false;
-	}
-
-	// Setting parmeters to context?
-	int error = avcodec_parameters_to_context(avctx, (*input_format_context).streams[videoStreamId]->codecpar);
-	if (error < 0)
-	{
-		avformat_close_input(&input_format_context);
-		avcodec_free_context(&avctx);
-		std::cout << "FFMpegGrab: Could not set parameters to context" << std::endl;
-		return false;
-	}
-	/// Open codec
-	if (avcodec_open2(avctx, input_codec, NULL) < 0)
-	{
-		std::cout << "FFMpegGrab: Could not open codec" << std::endl;
-		return false;
-	}
-
-	inputFrame = av_frame_alloc(); // Input
-	if (inputFrame == NULL)
-	{
-		std::cout << "FFMpegGrab: Could not allocate AVFrame" << std::endl;
-		return false;
-	}
-
-	// Outputframe
-	outputFrame = av_frame_alloc(); // Output (after resize and convertions)
-	outputFrame->format = AV_PIX_FMT_RGB24;
-	outputFrame->width = outputSizeX;
-	outputFrame->height = outputSizeY;
-
-	av_image_alloc(outputFrame->data, outputFrame->linesize, outputFrame->width, outputFrame->height, AV_PIX_FMT_RGB24, 32);
-
-	if (outputFrame == NULL)
-	{
-		std::cout << "FFMpegGrab: Could not allocate AVFrame" << std::endl;
-		return false;
-	}
-
-	// Function pointer and thread
-	typedef void (FFMpegGrab::*FuncMemFn)();
-	FuncMemFn p = &FFMpegGrab::loop;
-	std::thread(p, this).detach();
-
-	return true;
+FFMpegGrab::FFMpegGrab():
+    impl_(std::make_unique<Impl>())
+{
+    avformat_network_init();
+    av_log_set_level(AV_LOG_INFO);
 }
 
 FFMpegGrab::~FFMpegGrab()
 {
-	// Stop ffmpeg loop
-	shutdown = true;
-	while (!shutdownComplete)
-	{
-		sleep(1);
-	}
-	delete frame;
-	av_freep(&inputFrame->data[0]);
-	av_free(inputFrame);
-	av_freep(&outputFrame->data[0]);
-	av_free(outputFrame);
-	avcodec_free_context(&avctx);
-	avformat_close_input(&input_format_context);
+    {
+        std::lock_guard<std::mutex> lock(impl_->state_mutex);
+        impl_->shutdown = true;
+    }
+    impl_->state_cv.notify_all();
+
+    if(impl_->worker.joinable())
+        impl_->worker.join();
+
+    resetDecoderState();
 }
 
-void FFMpegGrab::loop()
+void
+FFMpegGrab::resetDecoderState()
 {
+    {
+        std::lock_guard<std::mutex> lock(impl_->state_mutex);
+        impl_->shutdown = true;
+    }
+    impl_->state_cv.notify_all();
 
+    if(impl_->worker.joinable())
+        impl_->worker.join();
+
+    if(impl_->img_convert_ctx)
+    {
+        sws_freeContext(impl_->img_convert_ctx);
+        impl_->img_convert_ctx = nullptr;
+    }
+
+    if(impl_->outputFrame)
+    {
+        av_freep(&impl_->outputFrame->data[0]);
+        av_frame_free(&impl_->outputFrame);
+    }
+
+    if(impl_->inputFrame)
+        av_frame_free(&impl_->inputFrame);
+
+    if(impl_->avctx)
+        avcodec_free_context(&impl_->avctx);
+
+    if(impl_->input_format_context)
+        avformat_close_input(&impl_->input_format_context);
+
+    impl_->videoStreamId = -1;
+    impl_->input_codec = nullptr;
+    impl_->frame.clear();
+    impl_->freshData = false;
+    impl_->shutdown = false;
+}
+
+void
+FFMpegGrab::SetOutputSize(int width, int height)
+{
+    impl_->outputSizeX = width;
+    impl_->outputSizeY = height;
+}
+
+void
+FFMpegGrab::SetUrl(const char * stream_url)
+{
+    impl_->url = stream_url ? stream_url : "";
+}
+
+void
+FFMpegGrab::SetPrintInfo(bool enabled)
+{
+    impl_->printInfo = enabled;
+}
+
+void
+FFMpegGrab::SetUv4l(bool enabled)
+{
+    impl_->uv4l = enabled;
+}
+
+void
+FFMpegGrab::SetSynchronized(bool enabled)
+{
+    impl_->synchronized = enabled;
+}
+
+bool
+FFMpegGrab::ReadFrame(uint8_t * destination, std::size_t bytes, bool wait_for_new_frame)
+{
+    std::unique_lock<std::mutex> lock(impl_->state_mutex);
+
+    if(wait_for_new_frame)
+    {
+        impl_->state_cv.wait(lock, [this] {
+            return impl_->shutdown || impl_->freshData;
+        });
+    }
+
+    if(!impl_->freshData || impl_->frame.empty())
+        return false;
+
+    if(bytes != impl_->frame.size())
+        throw std::runtime_error("FFMpegGrab::ReadFrame received a destination buffer of unexpected size.");
+
+    std::memcpy(destination, impl_->frame.data(), bytes);
+    impl_->freshData = false;
+    lock.unlock();
+    impl_->state_cv.notify_all();
+    return true;
+}
+
+bool
+FFMpegGrab::Init()
+{
+    if(impl_->printInfo)
+        std::cout << "FFMpegGrab Init()" << std::endl;
+
+    if(impl_->outputSizeX <= 0 || impl_->outputSizeY <= 0)
+    {
+        std::cout << "FFMpegGrab: Output size must be positive" << std::endl;
+        return false;
+    }
+
+    if(impl_->url.empty())
+    {
+        std::cout << "FFMpegGrab: Stream url is empty" << std::endl;
+        return false;
+    }
+
+    resetDecoderState();
+
+    const AVInputFormat * file_iformat = nullptr;
+    if(impl_->uv4l)
+        file_iformat = av_find_input_format("h264");
+
+    AVDictionary * options = nullptr;
+    if(impl_->uv4l)
+        av_dict_set(&options, "probesize", "192", 0);
+
+    if(avformat_open_input(&impl_->input_format_context, impl_->url.c_str(), file_iformat, &options) != 0)
+    {
+        std::cout << "FFMpegGrab: Could not open file " << impl_->url << std::endl;
+        av_dict_free(&options);
+        return false;
+    }
+    av_dict_free(&options);
+
+    if(av_find_best_stream(impl_->input_format_context, AVMEDIA_TYPE_VIDEO, 0, 0, nullptr, 0) < 0)
+    {
+        std::cout << "FFMpegGrab: Couldn't find stream information" << std::endl;
+        resetDecoderState();
+        return false;
+    }
+
+    if(impl_->printInfo)
+        av_dump_format(impl_->input_format_context, 0, impl_->url.c_str(), 0);
+
+    av_log_set_level(AV_LOG_ERROR);
+
+    impl_->videoStreamId = -1;
+    for(unsigned int i = 0; i < impl_->input_format_context->nb_streams; ++i)
+    {
+        if(impl_->input_format_context->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_VIDEO)
+        {
+            impl_->videoStreamId = static_cast<int>(i);
+            break;
+        }
+    }
+    if(impl_->videoStreamId == -1)
+    {
+        std::cout << "FFMpegGrab: Didn't find a video stream" << std::endl;
+        resetDecoderState();
+        return false;
+    }
+
+    if(impl_->uv4l)
+    {
+        impl_->input_codec = avcodec_find_decoder_by_name("h264_mmal");
+        if(impl_->input_codec)
+            std::cout << "FFMpegGrab: Using HW decoding (mmal)" << std::endl;
+        else
+            impl_->input_codec = avcodec_find_decoder(AV_CODEC_ID_H264);
+    }
+    else
+    {
+        impl_->input_codec = avcodec_find_decoder(
+            impl_->input_format_context->streams[impl_->videoStreamId]->codecpar->codec_id);
+    }
+
+    if(impl_->input_codec == nullptr)
+    {
+        std::cout << "FFMpegGrab: Unsupported codec!" << std::endl;
+        resetDecoderState();
+        return false;
+    }
+
+    impl_->avctx = avcodec_alloc_context3(impl_->input_codec);
+    if(!impl_->avctx)
+    {
+        std::cout << "FFMpegGrab: Could not allocate a decoding context" << std::endl;
+        return false;
+    }
+
+    int error = avcodec_parameters_to_context(
+        impl_->avctx,
+        impl_->input_format_context->streams[impl_->videoStreamId]->codecpar);
+    if(error < 0)
+    {
+        std::cout << "FFMpegGrab: Could not set parameters to context" << std::endl;
+        resetDecoderState();
+        return false;
+    }
+
+    if(avcodec_open2(impl_->avctx, impl_->input_codec, nullptr) < 0)
+    {
+        std::cout << "FFMpegGrab: Could not open codec" << std::endl;
+        resetDecoderState();
+        return false;
+    }
+
+    impl_->inputFrame = av_frame_alloc();
+    if(impl_->inputFrame == nullptr)
+    {
+        std::cout << "FFMpegGrab: Could not allocate AVFrame" << std::endl;
+        resetDecoderState();
+        return false;
+    }
+
+    impl_->outputFrame = av_frame_alloc();
+    if(impl_->outputFrame == nullptr)
+    {
+        std::cout << "FFMpegGrab: Could not allocate AVFrame" << std::endl;
+        resetDecoderState();
+        return false;
+    }
+
+    impl_->outputFrame->format = AV_PIX_FMT_RGB24;
+    impl_->outputFrame->width = impl_->outputSizeX;
+    impl_->outputFrame->height = impl_->outputSizeY;
+
+    if(av_image_alloc(impl_->outputFrame->data, impl_->outputFrame->linesize,
+                      impl_->outputFrame->width, impl_->outputFrame->height,
+                      AV_PIX_FMT_RGB24, 32) < 0)
+    {
+        std::cout << "FFMpegGrab: Could not allocate image buffer" << std::endl;
+        resetDecoderState();
+        return false;
+    }
+
+    impl_->frame.resize(static_cast<std::size_t>(impl_->outputSizeX) *
+                        static_cast<std::size_t>(impl_->outputSizeY) * 3);
+
+    impl_->worker = std::thread(&FFMpegGrab::loop, this);
+    return true;
+}
+
+void
+FFMpegGrab::loop()
+{
 #ifdef FFMPEG_TIMER
-	Timer timerSub;
+    Timer timerSub;
 #endif
 
-	std::mutex mtx; // mutex for critical section
+    int gotFrame = 0;
+    while(true)
+    {
+        {
+            std::lock_guard<std::mutex> lock(impl_->state_mutex);
+            if(impl_->shutdown)
+                break;
+        }
 
-	if (!frame)
-		frame = new uint8_t[outputSizeX * outputSizeY * 3 * sizeof(uint8_t)];
-
-	int gotFrame = 0;
-	while (true) // Always loop
-	{
-		if (av_read_frame(input_format_context, &packet) >= 0) // Read packet from source
-		{
-			if (packet.stream_index == videoStreamId) // Is this a packet from the video stream?
-			{
+        if(av_read_frame(impl_->input_format_context, &impl_->packet) >= 0)
+        {
+            if(impl_->packet.stream_index == impl_->videoStreamId)
+            {
 #ifdef FFMPEG_TIMER
-				timerSub.Restart();
+                timerSub.Restart();
 #endif
-				decode(avctx, inputFrame, &gotFrame, &packet);
-				if (gotFrame) // Decoder gave us a video frame
-				{
-
+                decode_packet(impl_->avctx, impl_->inputFrame, &gotFrame, &impl_->packet);
+                if(gotFrame)
+                {
 #ifdef FFMPEG_TIMER
-					std::cout << "FFMpegGrab (timing): Decoded Time\t" << round(timerSub.GetTime() / 1000) << " ms" << std::endl;
-					timerSub.Restart();
+                    std::cout << "FFMpegGrab (timing): Decoded Time\t"
+                              << round(timerSub.GetTime() / 1000) << " ms" << std::endl;
+                    timerSub.Restart();
 #endif
 
-					//	Convert the frame to AV_PIX_FMT_RGB24 format
-					img_convert_ctx = sws_getCachedContext(img_convert_ctx, avctx->width, avctx->height,
-														   avctx->pix_fmt,
-														   outputSizeX, outputSizeY, AV_PIX_FMT_RGB24,
-														   SWS_BICUBIC, NULL, NULL, NULL);
-#ifdef FFMPEG_TIMER
-					std::cout << "FFMpegGrab (timing): sws_getCachedC.. \t" << round(timerSub.GetTime() / 1000) << " ms" << std::endl;
-					timerSub.Restart();
-#endif
-					// Scale the image
-					sws_scale(img_convert_ctx, (const uint8_t *const *)inputFrame->data,
-							  inputFrame->linesize, 0, avctx->height,
-							  outputFrame->data, outputFrame->linesize);
+                    impl_->img_convert_ctx = sws_getCachedContext(
+                        impl_->img_convert_ctx,
+                        impl_->avctx->width, impl_->avctx->height,
+                        impl_->avctx->pix_fmt,
+                        impl_->outputSizeX, impl_->outputSizeY, AV_PIX_FMT_RGB24,
+                        SWS_BICUBIC, nullptr, nullptr, nullptr);
 
-					bool waitForCollected = true;
-#ifdef FFMPEG_TIMER
-					std::cout << "FFMpegGrab (timing): sws_scale Time\t" << round(timerSub.GetTime() / 1000) << " ms" << std::endl;
-					timerSub.Restart();
-#endif
-					mtx.lock();
-					freshData = true;
-					// Copy each row to have only the data sent to ikaros. This is because the linesize is not the same as the size of the image.
-					for (int y = 0; y < outputSizeY; y++)
-						memcpy(frame + y * outputSizeX * 3,
-							   outputFrame->data[0] + y * outputFrame->linesize[0],
-							   outputSizeX * 3);
-					mtx.unlock();
-#ifdef FFMPEG_TIMER
-					std::cout << "FFMpegGrab (timing): memcpy Time\t" << round(timerSub.GetTime() / 1000) << " ms" << std::endl;
-					timerSub.Restart();
-#endif
-					if (syncronized)
-						while (waitForCollected) // Wait unitil gotFrame is false ==  tick has taken the frame
-						{
-							usleep(1000);
-							mtx.lock();
-							waitForCollected = freshData; // If freshdata is set to false then assume data collected
-							mtx.unlock();
-						}
-#ifdef FFMPEG_TIMER
-					std::cout << "FFMpegGrab (timing): synchronized\t" << round(timerSub.GetTime() / 1000) << " ms\n" << std::endl;
-					timerSub.Restart();
-#endif
-					gotFrame = 0;
+                    if(impl_->img_convert_ctx == nullptr)
+                    {
+                        av_packet_unref(&impl_->packet);
+                        break;
+                    }
+
+                    sws_scale(impl_->img_convert_ctx, (const uint8_t *const *)impl_->inputFrame->data,
+                              impl_->inputFrame->linesize, 0, impl_->avctx->height,
+                              impl_->outputFrame->data, impl_->outputFrame->linesize);
+
+                    {
+                        std::unique_lock<std::mutex> lock(impl_->state_mutex);
+                        for(int y = 0; y < impl_->outputSizeY; ++y)
+                        {
+                            std::memcpy(impl_->frame.data() + static_cast<std::size_t>(y) * impl_->outputSizeX * 3,
+                                        impl_->outputFrame->data[0] + y * impl_->outputFrame->linesize[0],
+                                        static_cast<std::size_t>(impl_->outputSizeX) * 3);
+                        }
+                        impl_->freshData = true;
+                        lock.unlock();
+                        impl_->state_cv.notify_all();
+
+                        if(impl_->synchronized)
+                        {
+                            lock.lock();
+                            impl_->state_cv.wait(lock, [this] {
+                                return impl_->shutdown || !impl_->freshData;
+                            });
+                            if(impl_->shutdown)
+                                break;
+                        }
+                    }
+
+                    gotFrame = 0;
 #ifdef FFMPEG_FPS
-
-					FPS = FPS + (1.0 / timer.GetTime() - FPS) * 0.02; // Moving avarage mean FPS
-					timer.Restart();
-					std::cout << "FFMpegGrab " << url << ": FPS " << FPS << std::endl;
+                    impl_->FPS = impl_->FPS + (1.0 / impl_->timer.GetTime() - impl_->FPS) * 0.02f;
+                    impl_->timer.Restart();
+                    std::cout << "FFMpegGrab " << impl_->url << ": FPS " << impl_->FPS << std::endl;
 #endif
-					if (shutdown)
-					{
-						if (printInfo)
-							std::cout << "FFMpegGrab: Shutting down" << std::endl;
-						break;
-					}
-				}
-			}
-		}
-		av_packet_unref(&packet);
-	}
-	shutdownComplete = true;
-}
-
-int FFMpegGrab::decode(AVCodecContext *avctx, AVFrame *frame, int *got_frame, AVPacket *pkt)
-{
-	int ret;
-	*got_frame = 0;
-	if (pkt)
-	{
-		ret = avcodec_send_packet(avctx, pkt);
-		if (ret != 0)
-			return ret == AVERROR_EOF ? 0 : ret;
-	}
-	ret = avcodec_receive_frame(avctx, frame);
-	if (ret != 0 && ret != AVERROR(EAGAIN) && ret != AVERROR_EOF)
-		return ret;
-	if (ret == 0)
-		*got_frame = 1;
-	return 0;
+                }
+            }
+            av_packet_unref(&impl_->packet);
+        }
+    }
+    impl_->state_cv.notify_all();
 }
